922af93e0f236154a9ccda2c91accda99021adee
andreykolchev <andreykolchev@gmail.com>
2017-10-12 19:47:19 +0300

    cassandra properties

--- src/main/java/com/ocds/tender/TenderApplication.java
+++ src/main/java/com/ocds/tender/TenderApplication.java
@@ -1,13 +1,9 @@
 package com.ocds.tender;
 
-import com.ocds.tender.config.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
 
-@SpringBootApplication(scanBasePackageClasses = {ApplicationConfig.class})
-@EnableAutoConfiguration(exclude = {CassandraDataAutoConfiguration.class})
+@SpringBootApplication
 public class TenderApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/ocds/tender/config/CassandraConfig.java
+++ src/main/java/com/ocds/tender/config/CassandraConfig.java
@@ -1,16 +1,23 @@
 package com.ocds.tender.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
-import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
-@ComponentScan(basePackages = "com.ocds.tender.model.entity")
-@EnableCassandraRepositories(basePackages = "com.ocds.tender.repository")
-public class DatabaseConfig extends AbstractCassandraConfiguration {
+@EnableConfigurationProperties({CassandraProperties.class})
+public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    private CassandraProperties properties;
+
+    @Autowired
+    public CassandraConfig(CassandraProperties properties) {
+        this.properties = properties;
+    }
 
     @Override
     protected String getKeyspaceName() {
@@ -20,11 +27,11 @@ public class DatabaseConfig extends AbstractCassandraConfiguration {
     @Bean
     public CassandraClusterFactoryBean cluster() {
         CassandraClusterFactoryBean cluster = new CassandraClusterFactoryBean();
+        String username = properties.getUsername();
         cluster.setUsername("cassandra");
         cluster.setPassword("cassandra");
         cluster.setContactPoints("localhost");
         cluster.setPort(9042);
         return cluster;
     }
-
 }
