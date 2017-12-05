4004e532b6c0e6ad93aad51744e1483ae57f0a04
andreykolchev <andreykolchev@gmail.com>
2017-12-05 16:07:38 +0200

    remove dtos

--- src/main/java/com/ocds/access/model/pn/InitiationType.java
+++
@@ -1,5 +0,0 @@
-package com.ocds.access.model.pn;
-
-public enum InitiationType {
-    tender;
-}
--- src/main/java/com/ocds/access/model/pn/Planning.java
+++
@@ -1,6 +0,0 @@
-package com.ocds.access.model.pn;
-
-public class Planning {
-    public Budget budget;
-    public String rationale;
-}
