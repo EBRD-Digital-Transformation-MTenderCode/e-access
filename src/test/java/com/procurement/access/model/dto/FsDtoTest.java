507da652b60386166724fe68c0d45a3a5c5cfaee
andreykolchev <andreykolchev@gmail.com>
2018-02-12 11:44:24 +0200

    delete ein, fs logic

--- src/test/java/com/procurement/access/model/dto/FsDtoTest.java
+++ src/test/java/com/procurement/access/model/dto/FsDtoTest.java
@@ -1,8 +1,6 @@
 package com.procurement.access.model.dto;
 
 import com.procurement.access.AbstractDomainObjectTest;
-import com.procurement.access.model.dto.fs.FsDto;
-import org.junit.jupiter.api.Test;
 
 public class FsDtoTest extends AbstractDomainObjectTest {
 //    @Test
