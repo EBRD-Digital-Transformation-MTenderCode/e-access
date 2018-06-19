4c5565a0e048ea386ecc0e114450c139d219054b
Oleh Herashchenko <joghua@gmail.com>
2018-06-19 17:18:34 +0300

    json now ignore unknown prop

--- src/main/kotlin/com/procurement/access/config/ObjectMapperConfig.kt
+++ src/main/kotlin/com/procurement/access/config/ObjectMapperConfig.kt
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 
-
 @Configuration
 class ObjectMapperConfig {
 
@@ -15,9 +14,10 @@ class ObjectMapperConfig {
     @Primary
     fun objectMapper(): ObjectMapper {
         return ObjectMapper()
-                .configure(DeserializationFeature.USE_BIG_INTEGER_FOR_INTS, true)
-                .configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
-                .setNodeFactory(JsonNodeFactory.withExactBigDecimals(true))
+            .configure(DeserializationFeature.USE_BIG_INTEGER_FOR_INTS, true)
+            .configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .setNodeFactory(JsonNodeFactory.withExactBigDecimals(true))
     }
 
 }
