aa813a2f76d0749b1d6766b7969ef728c3b3196b
andreykolchev <andreykolchev@gmail.com>
2018-07-04 15:13:19 +0300

    dto validation

--- src/main/kotlin/com/procurement/access/model/dto/ocds/Option.kt
+++ src/main/kotlin/com/procurement/access/model/dto/ocds/Option.kt
@@ -1,8 +1,10 @@
 package com.procurement.access.model.dto.ocds
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class Option @JsonCreator constructor(
 
         @get:JsonProperty("hasOptions")
--- src/main/kotlin/com/procurement/access/model/dto/ocds/ProcedureOutsourcing.kt
+++ src/main/kotlin/com/procurement/access/model/dto/ocds/ProcedureOutsourcing.kt
@@ -1,9 +1,11 @@
 package com.procurement.access.model.dto.ocds
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import javax.validation.constraints.NotNull
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class ProcedureOutsourcing @JsonCreator constructor(
 
         @field:NotNull
