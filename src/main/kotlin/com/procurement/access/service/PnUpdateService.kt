7a20be8b00aaf590d97230ebe86e5cdf01a68c08
andreykolchev <andreykolchev@gmail.com>
2018-08-15 21:10:33 +0300

    check token

--- src/main/kotlin/com/procurement/access/service/PnUpdateService.kt
+++ src/main/kotlin/com/procurement/access/service/PnUpdateService.kt
@@ -93,9 +93,10 @@ class PnUpdateServiceImpl(private val generationService: GenerationService,
 
     private fun validateLotsAndItemsAndDocuments(pnDto: PnUpdate, currency: String) {
         if (pnDto.tender.lots == null) throw ErrorException(ErrorType.INVALID_ITEMS_RELATED_LOTS)
+        if (pnDto.tender.items == null) throw ErrorException(ErrorType.INVALID_ITEMS_RELATED_LOTS)
         checkLotsCurrency(pnDto.tender.lots, currency)
         checkLotsContractPeriod(pnDto.tender.lots)
-        validateRelatedLots(pnDto.tender.lots, pnDto.tender.items!!, pnDto.tender.documents)
+        validateRelatedLots(pnDto.tender.lots, pnDto.tender.items, pnDto.tender.documents)
     }
 
     private fun setContractPeriod(tender: Tender, activeLots: List<Lot>, budget: Budget) {
