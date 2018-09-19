32d2edbf01341cecd180a49c094820e3532549e8
andreykolchev <andreykolchev@gmail.com>
2018-09-19 19:16:11 +0300

    command

--- src/main/kotlin/com/procurement/access/controller/CommandController.kt
+++ src/main/kotlin/com/procurement/access/controller/CommandController.kt
@@ -3,8 +3,7 @@ package com.procurement.access.controller
 import com.procurement.access.exception.EnumException
 import com.procurement.access.exception.ErrorException
 import com.procurement.access.model.bpe.*
-import com.procurement.access.model.bpe.CommandType.*
-import com.procurement.access.service.*
+import com.procurement.access.service.CommandService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
@@ -13,53 +12,11 @@ import org.springframework.web.bind.annotation.*
 @Validated
 @RestController
 @RequestMapping("/command")
-class CommandController(private val pinService: PinService,
-                        private val pinOnPnService: PinOnPnService,
-                        private val pnService: PnService,
-                        private val pnUpdateService: PnUpdateService,
-                        private val cnCreateService: CnCreateService,
-                        private val cnUpdateService: CnUpdateService,
-                        private val cnOnPinService: CnOnPinService,
-                        private val cnOnPnService: CnOnPnService,
-                        private val tenderService: TenderService,
-                        private val lotsService: LotsService,
-                        private val stageService: StageService,
-                        private val validationService: ValidationService) {
+class CommandController(private val commandService: CommandService) {
 
     @PostMapping
-    fun command(@RequestBody commandMessage: CommandMessage): ResponseEntity<ResponseDto> {
-        return ResponseEntity(execute(commandMessage), HttpStatus.OK)
-    }
-
-    fun execute(cm: CommandMessage): ResponseDto {
-        return when (cm.command) {
-            CREATE_PIN -> pinService.createPin(cm)
-            CREATE_PN -> pnService.createPn(cm)
-            UPDATE_PN -> pnUpdateService.updatePn(cm)
-            CREATE_CN -> cnCreateService.createCn(cm)
-            UPDATE_CN -> cnUpdateService.updateCn(cm)
-            CREATE_PIN_ON_PN -> pinOnPnService.createPinOnPn(cm)
-            CREATE_CN_ON_PIN -> cnOnPinService.createCnOnPin(cm)
-            CREATE_CN_ON_PN -> cnOnPnService.createCnOnPn(cm)
-
-            SUSPEND_TENDER -> tenderService.suspendTender(cm)
-            UNSUSPEND_TENDER -> tenderService.unsuspendTender(cm)
-            UNSUCCESSFUL_TENDER -> tenderService.setUnsuccessful(cm)
-            PREPARE_CANCELLATION -> tenderService.prepareCancellation(cm)
-            TENDER_CANCELLATION -> tenderService.tenderCancellation(cm)
-            START_NEW_STAGE -> stageService.startNewStage(cm)
-
-            GET_LOTS -> lotsService.getLots(cm)
-            UPDATE_LOT_STATUS_DETAILS -> lotsService.updateStatusDetails(cm)
-            UPDATE_LOT_STATUS_DETAILS_BY_BID -> lotsService.updateStatusDetailsById(cm)
-            UPDATE_LOTS -> lotsService.updateLots(cm)
-            UPDATE_LOTS_EV -> lotsService.updateLotsEv(cm)
-            CHECK_LOT_STATUS -> lotsService.checkStatus(cm)
-            CHECK_LOT_GET_ITEMS -> lotsService.checkStatusDetailsGetItems(cm)
-            CHECK_BID -> validationService.checkBid(cm)
-            CHECK_ITEMS -> validationService.checkItems(cm)
-            CHECK_TOKEN -> validationService.checkToken(cm)
-        }
+    fun command(@RequestBody cm: CommandMessage): ResponseEntity<ResponseDto> {
+        return ResponseEntity(commandService.execute(cm), HttpStatus.OK)
     }
 
     @ResponseBody
---
+++ src/main/kotlin/com/procurement/access/service/CommandService.kt
@@ -0,0 +1,69 @@
+package com.procurement.access.service
+
+import com.procurement.access.dao.HistoryDao
+import com.procurement.access.model.bpe.CommandMessage
+import com.procurement.access.model.bpe.CommandType
+import com.procurement.access.model.bpe.ResponseDto
+import com.procurement.access.utils.toObject
+import org.springframework.stereotype.Service
+
+interface CommandService {
+
+    fun execute(cm: CommandMessage): ResponseDto
+
+}
+
+@Service
+class CommandServiceImpl(private val historyDao: HistoryDao,
+                         private val pinService: PinService,
+                         private val pinOnPnService: PinOnPnService,
+                         private val pnService: PnService,
+                         private val pnUpdateService: PnUpdateService,
+                         private val cnCreateService: CnCreateService,
+                         private val cnUpdateService: CnUpdateService,
+                         private val cnOnPinService: CnOnPinService,
+                         private val cnOnPnService: CnOnPnService,
+                         private val tenderService: TenderService,
+                         private val lotsService: LotsService,
+                         private val stageService: StageService,
+                         private val validationService: ValidationService) : CommandService {
+
+
+    override fun execute(cm: CommandMessage): ResponseDto {
+        var historyEntity = historyDao.getHistory(cm.context.operationId, cm.command.value())
+        if (historyEntity != null) {
+            return toObject(ResponseDto::class.java, historyEntity.jsonData)
+        }
+        val response = when (cm.command) {
+            CommandType.CREATE_PIN -> pinService.createPin(cm)
+            CommandType.CREATE_PN -> pnService.createPn(cm)
+            CommandType.UPDATE_PN -> pnUpdateService.updatePn(cm)
+            CommandType.CREATE_CN -> cnCreateService.createCn(cm)
+            CommandType.UPDATE_CN -> cnUpdateService.updateCn(cm)
+            CommandType.CREATE_PIN_ON_PN -> pinOnPnService.createPinOnPn(cm)
+            CommandType.CREATE_CN_ON_PIN -> cnOnPinService.createCnOnPin(cm)
+            CommandType.CREATE_CN_ON_PN -> cnOnPnService.createCnOnPn(cm)
+
+            CommandType.SUSPEND_TENDER -> tenderService.suspendTender(cm)
+            CommandType.UNSUSPEND_TENDER -> tenderService.unsuspendTender(cm)
+            CommandType.UNSUCCESSFUL_TENDER -> tenderService.setUnsuccessful(cm)
+            CommandType.PREPARE_CANCELLATION -> tenderService.prepareCancellation(cm)
+            CommandType.TENDER_CANCELLATION -> tenderService.tenderCancellation(cm)
+            CommandType.START_NEW_STAGE -> stageService.startNewStage(cm)
+
+            CommandType.GET_LOTS -> lotsService.getLots(cm)
+            CommandType.UPDATE_LOT_STATUS_DETAILS -> lotsService.updateStatusDetails(cm)
+            CommandType.UPDATE_LOT_STATUS_DETAILS_BY_BID -> lotsService.updateStatusDetailsById(cm)
+            CommandType.UPDATE_LOTS -> lotsService.updateLots(cm)
+            CommandType.UPDATE_LOTS_EV -> lotsService.updateLotsEv(cm)
+            CommandType.CHECK_LOT_STATUS -> lotsService.checkStatus(cm)
+            CommandType.CHECK_LOT_GET_ITEMS -> lotsService.checkStatusDetailsGetItems(cm)
+            CommandType.CHECK_BID -> validationService.checkBid(cm)
+            CommandType.CHECK_ITEMS -> validationService.checkItems(cm)
+            CommandType.CHECK_TOKEN -> validationService.checkToken(cm)
+        }
+        historyEntity = historyDao.saveHistory(cm.context.operationId, cm.command.value(), response)
+        return toObject(ResponseDto::class.java, historyEntity.jsonData)
+    }
+
+}
