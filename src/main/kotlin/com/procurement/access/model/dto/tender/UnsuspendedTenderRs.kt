ac63eff057fddb8dd44ff35430b28883a771cd9c
andreykolchev <andreykolchev@gmail.com>
2018-10-17 16:24:48 +0300

    add rules

---
+++ src/main/kotlin/com/procurement/access/model/dto/tender/UnsuspendedTenderRs.kt
@@ -0,0 +1,23 @@
+package com.procurement.access.model.dto.tender
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.procurement.access.model.dto.ocds.ElectronicAuctions
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class UnsuspendedTenderRs @JsonCreator constructor(
+
+        val tender: UnsuspendedTender
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class UnsuspendedTender @JsonCreator constructor(
+
+        val status: String?,
+
+        val statusDetails: String?,
+
+        val procurementMethodModalities: Set<String>?,
+
+        val electronicAuctions: ElectronicAuctions?
+)
--- src/main/kotlin/com/procurement/access/model/dto/tender/UpdateTenderStatusRs.kt
+++ src/main/kotlin/com/procurement/access/model/dto/tender/UpdateTenderStatusRs.kt
@@ -1,4 +1,4 @@
-package com.procurement.access.model.dto.cn
+package com.procurement.access.model.dto.tender
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonInclude
