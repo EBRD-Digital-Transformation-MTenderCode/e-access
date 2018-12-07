b559d6b757f0a7b9f32670f2086d04668116de3f
andreykolchev <andreykolchev@gmail.com>
2018-12-07 19:54:14 +0200

    complete lot

---
+++ src/main/kotlin/com/procurement/access/model/dto/lots/ActivationAc.kt
@@ -0,0 +1,42 @@
+package com.procurement.access.model.dto.lots
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.procurement.access.model.dto.ocds.LotStatus
+import com.procurement.access.model.dto.ocds.LotStatusDetails
+import com.procurement.access.model.dto.ocds.TenderStatus
+import com.procurement.access.model.dto.ocds.TenderStatusDetails
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class ActivationAcRq @JsonCreator constructor(
+
+        val lotId: String,
+
+        val stageEnd:Boolean
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class ActivationAcRs @JsonCreator constructor(
+
+        val tender:ActivationAcRsTender,
+
+        val lot:ActivationAcRsLot
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class ActivationAcRsTender @JsonCreator constructor(
+
+        var status: TenderStatus,
+
+        var statusDetails: TenderStatusDetails
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class ActivationAcRsLot @JsonCreator constructor(
+
+        val id: String,
+
+        var status: LotStatus? = null,
+
+        var statusDetails: LotStatusDetails?
+)
