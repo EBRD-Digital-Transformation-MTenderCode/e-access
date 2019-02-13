f1b53535f058aa967528fa9b075e9be096ae12ed
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-02-13 11:21:27 +0200

    Added the JSON Deserializer & Serializer for Period DTO

--- src/main/kotlin/com/procurement/access/model/dto/ocds/Period.kt
+++ src/main/kotlin/com/procurement/access/model/dto/ocds/Period.kt
@@ -2,12 +2,19 @@ package com.procurement.access.model.dto.ocds
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.procurement.access.model.dto.databinding.JsonDateTimeDeserializer
+import com.procurement.access.model.dto.databinding.JsonDateTimeSerializer
 import java.time.LocalDateTime
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Period @JsonCreator constructor(
+    @JsonDeserialize(using = JsonDateTimeDeserializer::class)
+    @JsonSerialize(using = JsonDateTimeSerializer::class)
+    val startDate: LocalDateTime,
 
-        val startDate: LocalDateTime,
-
-        val endDate: LocalDateTime?
+    @JsonDeserialize(using = JsonDateTimeDeserializer::class)
+    @JsonSerialize(using = JsonDateTimeSerializer::class)
+    val endDate: LocalDateTime?
 )
