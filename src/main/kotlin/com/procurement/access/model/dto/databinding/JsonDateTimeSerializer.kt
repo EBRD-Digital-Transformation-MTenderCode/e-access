84495aad076b6f0e74ce5fd7bafa5d1435b3b941
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-02-13 11:20:57 +0200

    Fixed the JSON Deserializer & Serializer for the DateTime type

--- src/main/kotlin/com/procurement/access/model/dto/databinding/JsonDateTimeSerializer.kt
+++ src/main/kotlin/com/procurement/access/model/dto/databinding/JsonDateTimeSerializer.kt
@@ -1,4 +1,4 @@
-package com.procurement.access.databinding
+package com.procurement.access.model.dto.databinding
 
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonProcessingException
@@ -8,23 +8,25 @@ import java.io.IOException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatterBuilder
+import java.time.format.ResolverStyle
 import java.time.temporal.ChronoField
 
-class JsonDateSerializer : JsonSerializer<LocalDateTime>() {
+class JsonDateTimeSerializer : JsonSerializer<LocalDateTime>() {
 
     object DateFormatter {
         val formatter: DateTimeFormatter = DateTimeFormatterBuilder()
-                .parseCaseInsensitive()
-                .append(DateTimeFormatter.ISO_LOCAL_DATE)
-                .appendLiteral('T')
-                .appendValue(ChronoField.HOUR_OF_DAY, 2)
-                .appendLiteral(':')
-                .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
-                .optionalStart()
-                .appendLiteral(':')
-                .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
-                .appendLiteral('Z')
-                .toFormatter()
+            .parseCaseInsensitive()
+            .append(DateTimeFormatter.ISO_LOCAL_DATE)
+            .appendLiteral('T')
+            .appendValue(ChronoField.HOUR_OF_DAY, 2)
+            .appendLiteral(':')
+            .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+            .optionalStart()
+            .appendLiteral(':')
+            .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+            .appendLiteral('Z')
+            .toFormatter()
+            .withResolverStyle(ResolverStyle.STRICT)
     }
 
     @Throws(IOException::class, JsonProcessingException::class)
