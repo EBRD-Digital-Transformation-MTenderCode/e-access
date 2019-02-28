7725a1be14464cbf4e7e7b788ff03bf0fa60fc62
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-02-28 11:31:03 +0200

    Fixed the package of the MoneyDeserializer and fixed imports in dependency files.

--- src/main/kotlin/com/procurement/access/model/dto/ocds/Value.kt
+++ src/main/kotlin/com/procurement/access/model/dto/ocds/Value.kt
@@ -3,7 +3,7 @@ package com.procurement.access.model.dto.ocds
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import com.procurement.access.databinding.MoneyDeserializer
+import com.procurement.access.model.dto.databinding.MoneyDeserializer
 import java.math.BigDecimal
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
