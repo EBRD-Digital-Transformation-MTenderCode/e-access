f1ff87a2cfc5015009f202c6076aa6abfaf94379
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-04-22 15:12:08 +0300

    Fix showing message of an error.

--- src/main/kotlin/com/procurement/access/exception/ErrorException.kt
+++ src/main/kotlin/com/procurement/access/exception/ErrorException.kt
@@ -3,7 +3,7 @@ package com.procurement.access.exception
 class ErrorException(val error: ErrorType, message: String? = null) :
     RuntimeException(
         if (message != null)
-            error.message + message
+            "${error.message} $message"
         else
             error.message
     )
