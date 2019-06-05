c55d24da3b80c16b820761af21b0ca01f9add51c
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-06-05 13:05:34 +0300

    Added logging to command controller.

--- src/main/kotlin/com/procurement/access/controller/CommandController.kt
+++ src/main/kotlin/com/procurement/access/controller/CommandController.kt
@@ -8,6 +8,10 @@ import com.procurement.access.model.dto.bpe.getEnumExceptionResponseDto
 import com.procurement.access.model.dto.bpe.getErrorExceptionResponseDto
 import com.procurement.access.model.dto.bpe.getExceptionResponseDto
 import com.procurement.access.service.CommandService
+import com.procurement.access.utils.toJson
+import com.procurement.access.utils.toObject
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
@@ -23,10 +27,20 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/command")
 class CommandController(private val commandService: CommandService) {
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(CommandController::class.java)
+    }
 
     @PostMapping
-    fun command(@RequestBody cm: CommandMessage): ResponseEntity<ResponseDto> {
-        return ResponseEntity(commandService.execute(cm), HttpStatus.OK)
+    fun command(@RequestBody requestBody: String): ResponseEntity<ResponseDto> {
+        if (log.isDebugEnabled)
+            log.debug("RECEIVED COMMAND: '$requestBody'.")
+        val cm: CommandMessage = toObject(CommandMessage::class.java, requestBody)
+
+        val response = commandService.execute(cm)
+        if (log.isDebugEnabled)
+            log.debug("RESPONSE (operation-id: '${cm.context.operationId}'): '${toJson(response)}'.")
+        return ResponseEntity(response, HttpStatus.OK)
     }
 
     @ResponseBody
@@ -40,6 +54,3 @@ class CommandController(private val commandService: CommandService) {
         }
     }
 }
-
-
-
