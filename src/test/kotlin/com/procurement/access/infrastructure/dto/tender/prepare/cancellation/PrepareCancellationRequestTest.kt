b944a19c1538396866d92a10af19d9244bf3dfdb
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-08-19 15:27:45 +0300

    ES-1782 Added DTO 'PrepareCancellationRequest'.

---
+++ src/test/kotlin/com/procurement/access/infrastructure/dto/tender/prepare/cancellation/PrepareCancellationRequestTest.kt
@@ -0,0 +1,23 @@
+package com.procurement.access.infrastructure.dto.tender.prepare.cancellation
+
+import com.procurement.access.infrastructure.AbstractDTOTestBase
+import org.junit.jupiter.api.Test
+
+class PrepareCancellationRequestTest :
+    AbstractDTOTestBase<PrepareCancellationRequest>(PrepareCancellationRequest::class.java) {
+
+    @Test
+    fun test() {
+        testBindingAndMapping("json/dto/tender/prepare/cancellation/request/request_prepare_cancellation_fully.json")
+    }
+
+    @Test
+    fun required1() {
+        testBindingAndMapping("json/dto/tender/prepare/cancellation/request/request_prepare_cancellation_required_1.json")
+    }
+
+    @Test
+    fun required2() {
+        testBindingAndMapping("json/dto/tender/prepare/cancellation/request/request_prepare_cancellation_required_2.json")
+    }
+}
