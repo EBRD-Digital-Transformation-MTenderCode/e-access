e48d8cdacaa0c1a94f0bc886a609c23c508822d5
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-08-19 15:26:26 +0300

    ES-1782 Added new package for scanning of Spring.

--- src/main/kotlin/com/procurement/access/config/ServiceConfig.kt
+++ src/main/kotlin/com/procurement/access/config/ServiceConfig.kt
@@ -4,5 +4,10 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ComponentScan(basePackages = ["com.procurement.access.service"])
+@ComponentScan(
+    basePackages = [
+        "com.procurement.access.service",
+        "com.procurement.access.application.service"
+    ]
+)
 class ServiceConfig
