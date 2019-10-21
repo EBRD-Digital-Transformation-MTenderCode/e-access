a2c5f1dea93ce8f612a01671a14b3e7bb1be2c22
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-10-21 14:32:29 +0300

    ES-2022 Changed structure of the properties of the OCDS.

--- src/main/kotlin/com/procurement/access/config/OCDSProperties.kt
+++ src/main/kotlin/com/procurement/access/config/OCDSProperties.kt
@@ -3,7 +3,11 @@ package com.procurement.access.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "ocds")
-class OCDSProperties {
-    var prefix: String? = null
+class OCDSProperties(
+    var prefixes: Prefixes? = null
+) {
+    class Prefixes(
+        var main: String? = null,
+        var test: String? = null
+    )
 }
-
