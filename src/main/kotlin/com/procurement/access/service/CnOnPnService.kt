87d1cf3e392655cda1384c9598ee205fd61e6284
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-10-30 11:32:26 +0200

    ES-2019 Fixed name of attribute (AwardCriteriaDetails -> awardCriteriaDetails).

--- src/main/kotlin/com/procurement/access/service/CnOnPnService.kt
+++ src/main/kotlin/com/procurement/access/service/CnOnPnService.kt
@@ -1041,7 +1041,7 @@ class CnOnPnService(
 
             //BR-3.8.17 -> BR-3.6.22 | VR-3.6.16
             awardCriteria = request.tender.awardCriteria,
-            awardCriteriaDetails = request.tender.AwardCriteriaDetails,
+            awardCriteriaDetails = request.tender.awardCriteriaDetails,
             tenderPeriod = request.tender.tenderPeriod.let { period ->
                 CNEntity.Tender.TenderPeriod(
                     startDate = period.startDate,
