0f8a6589edcefbc2064ee80d812825b79f754d9a
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-11-12 12:53:57 +0200

    ES-2186 Refactored and improved the configuration of the 'ObjectMapper' object which does parsing a JSON.

--- src/main/kotlin/com/procurement/access/config/ObjectMapperConfig.kt
+++ src/main/kotlin/com/procurement/access/config/ObjectMapperConfig.kt
@@ -1,32 +1,14 @@
 package com.procurement.access.config
 
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.module.SimpleModule
-import com.fasterxml.jackson.databind.node.JsonNodeFactory
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.procurement.access.model.dto.databinding.IntDeserializer
-import com.procurement.access.model.dto.databinding.JsonDateTimeDeserializer
-import com.procurement.access.model.dto.databinding.JsonDateTimeSerializer
-import com.procurement.access.model.dto.databinding.StringsDeserializer
+import com.procurement.access.infrastructure.bind.jackson.configuration
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
-import java.time.LocalDateTime
 
 @Configuration
 class ObjectMapperConfig(@Autowired objectMapper: ObjectMapper) {
 
     init {
-        val module = SimpleModule()
-        module.addSerializer(LocalDateTime::class.java, JsonDateTimeSerializer())
-        module.addDeserializer(LocalDateTime::class.java, JsonDateTimeDeserializer())
-        module.addDeserializer(String::class.java, StringsDeserializer())
-        module.addDeserializer(Int::class.java, IntDeserializer())
-        objectMapper.registerModule(module)
-        objectMapper.registerModule(KotlinModule())
-        objectMapper.configure(DeserializationFeature.USE_BIG_INTEGER_FOR_INTS, true)
-        objectMapper.configure(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS, true)
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        objectMapper.nodeFactory = JsonNodeFactory.withExactBigDecimals(true)
+        objectMapper.configuration()
     }
 }
