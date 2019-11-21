6547903a74de079b35c4d0377805fbc29c93048f
Evgen Kopach <zkopach@gmail.com>
2019-11-21 16:38:12 +0200

    ES-2183 Created service dto for 'getLotsAuction' command

---
+++ src/main/kotlin/com/procurement/access/application/model/data/GetLotsAuctionResponseData.kt
@@ -0,0 +1,21 @@
+package com.procurement.access.application.model.data
+
+import com.procurement.access.domain.model.money.Money
+
+data class GetLotsAuctionResponseData(
+    val tender: Tender
+) {
+    data class Tender(
+        val id: String,
+        val title: String,
+        val description: String,
+        val lots: List<Lot>
+    ) {
+        data class Lot(
+            val id: String,
+            val title: String,
+            val description: String,
+            val value: Money
+        )
+    }
+}
