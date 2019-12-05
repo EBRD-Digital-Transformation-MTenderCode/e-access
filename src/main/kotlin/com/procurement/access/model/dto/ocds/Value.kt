e9e2b13850a39292635977e9d8459e11840a8e6b
Evgen Kopach <zkopach@gmail.com>
2019-12-05 12:02:39 +0200

    ES-2183 Added method Money.asValue & Value.asMoney

--- src/main/kotlin/com/procurement/access/model/dto/ocds/Value.kt
+++ src/main/kotlin/com/procurement/access/model/dto/ocds/Value.kt
@@ -3,14 +3,31 @@ package com.procurement.access.model.dto.ocds
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.procurement.access.domain.model.money.Money
 import com.procurement.access.model.dto.databinding.MoneyDeserializer
 import java.math.BigDecimal
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Value @JsonCreator constructor(
 
-        @field:JsonDeserialize(using = MoneyDeserializer::class)
-        var amount: BigDecimal,
+    @field:JsonDeserialize(using = MoneyDeserializer::class)
+    var amount: BigDecimal,
 
-        var currency: String
+    var currency: String
 )
+
+val Money.asValue: Value
+    get() = this.let { money ->
+        Value(
+            amount = money.amount,
+            currency = money.currency
+        )
+    }
+
+val Value.asMoney: Money
+    get() = this.let { money ->
+        Money(
+            amount = money.amount,
+            currency = money.currency
+        )
+    }
