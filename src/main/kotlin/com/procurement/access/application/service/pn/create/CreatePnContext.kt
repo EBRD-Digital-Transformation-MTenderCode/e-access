ce526841eba2a383c103b1dd9be0a229fe321bc3
Evgen Kopach <zkopach@gmail.com>
2019-12-21 19:31:06 +0200

    ES-2038 Created context for 'createPn' command

---
+++ src/main/kotlin/com/procurement/access/application/service/pn/create/CreatePnContext.kt
@@ -0,0 +1,15 @@
+package com.procurement.access.application.service.pn.create
+
+import com.procurement.access.application.model.Mode
+import com.procurement.access.domain.model.enums.ProcurementMethod
+import java.time.LocalDateTime
+
+data class CreatePnContext(
+    val stage: String,
+    val owner: String,
+    val country: String,
+    val pmd: ProcurementMethod,
+    val startDate: LocalDateTime,
+    val mode: Mode,
+    val operationId: String
+)
