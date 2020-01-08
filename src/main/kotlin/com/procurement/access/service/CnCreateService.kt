eb15f41085767cf137b29ec85d0a5ae13ba16492
Evgen Kopach <zkopach@gmail.com>
2020-01-08 16:55:36 +0200

    ES-2038 Passed context to createCn method

--- src/main/kotlin/com/procurement/access/service/CnCreateService.kt
+++ src/main/kotlin/com/procurement/access/service/CnCreateService.kt
@@ -1,5 +1,6 @@
 package com.procurement.access.service
 
+import com.procurement.access.application.service.cn.update.CnCreateContext
 import com.procurement.access.dao.TenderProcessDao
 import com.procurement.access.domain.model.enums.AwardCriteria
 import com.procurement.access.domain.model.enums.LotStatus
@@ -10,7 +11,6 @@ import com.procurement.access.domain.model.enums.TenderStatus.ACTIVE
 import com.procurement.access.domain.model.enums.TenderStatusDetails
 import com.procurement.access.exception.ErrorException
 import com.procurement.access.exception.ErrorType
-import com.procurement.access.exception.ErrorType.CONTEXT
 import com.procurement.access.exception.ErrorType.INVALID_AUCTION_CURRENCY
 import com.procurement.access.exception.ErrorType.INVALID_AUCTION_ID
 import com.procurement.access.exception.ErrorType.INVALID_AUCTION_MINIMUM
@@ -26,7 +26,6 @@ import com.procurement.access.exception.ErrorType.INVALID_LOT_ID
 import com.procurement.access.exception.ErrorType.INVALID_PMD
 import com.procurement.access.model.dto.bpe.CommandMessage
 import com.procurement.access.model.dto.bpe.ResponseDto
-import com.procurement.access.model.dto.bpe.testMode
 import com.procurement.access.model.dto.cn.BudgetCnCreate
 import com.procurement.access.model.dto.cn.CnCreate
 import com.procurement.access.model.dto.cn.ItemCnCreate
@@ -59,7 +58,6 @@ import com.procurement.access.model.dto.ocds.validate
 import com.procurement.access.model.entity.TenderProcessEntity
 import com.procurement.access.utils.toDate
 import com.procurement.access.utils.toJson
-import com.procurement.access.utils.toLocal
 import com.procurement.access.utils.toObject
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
@@ -71,20 +69,13 @@ class CnCreateService(private val generationService: GenerationService,
                       private val tenderProcessDao: TenderProcessDao,
                       private val rulesService: RulesService) {
 
-    fun createCn(cm: CommandMessage): ResponseDto {
-        val country = cm.context.country ?: throw ErrorException(CONTEXT)
-        val pmd: ProcurementMethod = cm.context.pmd?.let { getPmd(it) } ?: throw ErrorException(CONTEXT)
-        val owner = cm.context.owner ?: throw ErrorException(CONTEXT)
-        val stage = cm.context.stage ?: throw ErrorException(CONTEXT)
-        val dateTime = cm.context.startDate?.toLocal() ?: throw ErrorException(CONTEXT)
-        val phase = cm.context.phase ?: throw ErrorException(CONTEXT)
-        val testMode = cm.testMode
+    fun createCn(cm: CommandMessage, context: CnCreateContext): ResponseDto {
         val cnDto = toObject(CnCreate::class.java, cm.data).validate()
-        validateAuctionsDto(country, pmd, cnDto)
+        validateAuctionsDto(context.country, context.pmd, cnDto)
 
         checkLotsCurrency(cnDto)
         checkLotsContractPeriod(cnDto)
-        val cpId = generationService.getCpId(country = country, testMode = testMode)
+        val cpId = generationService.getCpId(country = context.country, mode = context.mode)
         val planningDto = cnDto.planning
         val tenderDto = cnDto.tender
         validateDtoRelatedLots(tenderDto)
@@ -108,11 +99,11 @@ class CnCreateService(private val generationService: GenerationService,
                     title = tenderDto.title,
                     description = tenderDto.description,
                     status = ACTIVE,
-                    statusDetails = TenderStatusDetails.fromString(phase),
+                    statusDetails = TenderStatusDetails.fromString(context.phase),
                     classification = tenderDto.classification,
                     mainProcurementCategory = tenderDto.mainProcurementCategory,
                     additionalProcurementCategories = null,
-                    procurementMethod = pmd,
+                    procurementMethod = context.pmd,
                     procurementMethodDetails = tenderDto.procurementMethodDetails,
                     procurementMethodRationale = tenderDto.procurementMethodRationale,
                     procurementMethodAdditionalInfo = tenderDto.procurementMethodAdditionalInfo,
@@ -144,7 +135,7 @@ class CnCreateService(private val generationService: GenerationService,
                     electronicAuctions = tenderDto.electronicAuctions
                 )
         )
-        val entity = getEntity(tp, cpId, stage, dateTime, owner)
+        val entity = getEntity(tp, cpId, context.stage, context.startDate, context.owner)
         tenderProcessDao.save(entity)
         tp.token = entity.token.toString()
         return ResponseDto(data = tp)
