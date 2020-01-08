0a82cf154343201b00e67d28d3133448ae8c0f6f
Evgen Kopach <zkopach@gmail.com>
2020-01-08 16:56:26 +0200

    ES-2038 Added phase property to command message

--- src/main/kotlin/com/procurement/access/model/dto/bpe/CommandMessage.kt
+++ src/main/kotlin/com/procurement/access/model/dto/bpe/CommandMessage.kt
@@ -53,9 +53,9 @@ val CommandMessage.country: String
     get() = this.context.country
         ?: throw ErrorException(error = ErrorType.CONTEXT, message = "Missing the 'country' attribute in context.")
 
-val CommandMessage.operationId: String
-    get() = this.context.operationId
-        ?: throw ErrorException(error = ErrorType.CONTEXT, message = "Missing the 'operationId' attribute in context.")
+val CommandMessage.phase: String
+    get() = this.context.phase
+        ?: throw ErrorException(error = ErrorType.CONTEXT, message = "Missing the 'phase' attribute in context.")
 
 val CommandMessage.pmd: ProcurementMethod
     get() = this.context.pmd?.let {
