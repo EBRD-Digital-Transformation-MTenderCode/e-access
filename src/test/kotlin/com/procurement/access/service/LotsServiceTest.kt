5c20fcc1c187f7fe5f3e656e24d2f7c47cb6cd44
yevhenii <yevheniiskoryi@gmail.com>
2020-02-12 16:37:54 +0200

    ES-2515 refactored success test in LotsServiceTest

--- src/test/kotlin/com/procurement/access/service/LotsServiceTest.kt
+++ src/test/kotlin/com/procurement/access/service/LotsServiceTest.kt
@@ -3,15 +3,16 @@ package com.procurement.access.service
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
+import com.procurement.access.application.service.lot.GetLotIdsResult
 import com.procurement.access.dao.TenderProcessDao
+import com.procurement.access.domain.model.lot.LotId
 import com.procurement.access.infrastructure.dto.converter.convert
 import com.procurement.access.infrastructure.dto.lot.GetLotIdsRequest
 import com.procurement.access.infrastructure.generator.TenderProcessEntityGenerator
 import com.procurement.access.json.JSON
-import com.procurement.access.json.JsonValidator
 import com.procurement.access.json.loadJson
-import com.procurement.access.json.toJson
 import com.procurement.access.utils.toObject
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
@@ -22,7 +23,6 @@ class LotsServiceTest {
     companion object {
         private const val PATH_TO_REQUEST = "json/service/lots/getLotIds/get_lot_ids_full.json"
         private const val PATH_TO_TENDER_PROCESS = "json/service/lots/getLotIds/tender_process.json"
-        private const val PATH_TO_OUTPUT = "json/service/lots/getLotIds/expected_output.json"
     }
 
     private lateinit var tenderProcessDao: TenderProcessDao
@@ -39,13 +39,11 @@ class LotsServiceTest {
     inner class GetLotIds {
         private lateinit var request: JSON
         private lateinit var tenderProcess: JSON
-        private lateinit var output: JSON
 
         @BeforeEach
         fun prepare() {
             request = loadJson(PATH_TO_REQUEST)
             tenderProcess = loadJson(PATH_TO_TENDER_PROCESS)
-            output = loadJson(PATH_TO_OUTPUT)
             val tenderProcessEntity = TenderProcessEntityGenerator.generate(data = tenderProcess)
             whenever(tenderProcessDao.getByCpIdAndStage(cpId = any(), stage = any()))
                 .thenReturn(tenderProcessEntity)
@@ -58,8 +56,15 @@ class LotsServiceTest {
             val data = toObject(GetLotIdsRequest::class.java, request)
 
             val result = service.getLotIds(data = data.convert())
-
-            JsonValidator.equalsJsons(expectedJson = output, actualJson = result.convert().toJson())
+            val output = GetLotIdsResult(
+                lotIds = listOf(
+                    LotId.fromString("577fd5c4-e314-40a9-aabe-732d7f5269ad"),
+                    LotId.fromString("577fd5c4-e314-40a9-aabe-732d7f5269a3"),
+                    LotId.fromString("577fd5c4-e314-40a9-aabe-732d7f5269a4"),
+                    LotId.fromString("577fd5c4-e314-40a9-aabe-732d7f5269a5")
+                )
+            )
+            assertEquals(output, result)
         }
     }
 }
