79133cca255a043ce7006fa6989225cf38bb1448
yevhenii <yevheniiskoryi@gmail.com>
2020-02-12 16:32:31 +0200

    ES-2515 removed unuseful annotation in Command2Controller

--- src/main/kotlin/com/procurement/access/controller/Command2Controller.kt
+++ src/main/kotlin/com/procurement/access/controller/Command2Controller.kt
@@ -11,13 +11,11 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@Validated
 @RestController
 @RequestMapping("/command2")
 class Command2Controller(private val commandService2: CommandService2) {
