81e3d00c3f1ea3a15d0eea5c709b8d18e0e4ea26
yevhenii <yevheniiskoryi@gmail.com>
2020-03-03 16:00:47 +0200

    ES-2515 updated CommandService2 to use Result

--- src/main/kotlin/com/procurement/access/service/CommandService2.kt
+++ src/main/kotlin/com/procurement/access/service/CommandService2.kt
@@ -1,47 +1,31 @@
 package com.procurement.access.service
 
 import com.fasterxml.jackson.databind.JsonNode
-import com.procurement.access.dao.HistoryDao
-import com.procurement.access.infrastructure.handlers.GetLotIdsHandler
-import com.procurement.access.infrastructure.web.dto.ApiSuccessResponse
+import com.procurement.access.infrastructure.handler.get.lotids.GetLotIdsHandler
+import com.procurement.access.infrastructure.web.dto.ApiResponse
 import com.procurement.access.model.dto.bpe.Command2Type
 import com.procurement.access.model.dto.bpe.getAction
-import com.procurement.access.model.dto.bpe.getId
-import com.procurement.access.utils.toJson
-import com.procurement.access.utils.toObject
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class CommandService2(
-    private val historyDao: HistoryDao,
     private val getLotIdsHandler: GetLotIdsHandler
 ) {
     companion object {
         private val log = LoggerFactory.getLogger(CommandService2::class.java)
     }
 
-    fun execute(request: JsonNode): ApiSuccessResponse {
+    fun execute(request: JsonNode): ApiResponse {
 
-        val id = request.getId()
-        val action = request.getAction()
-
-        val historyEntity = historyDao.getHistory(id.toString(), action.toString())
-        if (historyEntity != null) {
-            return toObject(ApiSuccessResponse::class.java, historyEntity.jsonData)
-        }
-
-        val response: ApiSuccessResponse = when (action) {
+        val response = when (request.getAction().get) {
             Command2Type.GET_LOT_IDS -> {
-
-                val response = getLotIdsHandler.handle(request = request)
-                response.also {
-                    log.debug("GET_LOT_IDS. Response: ${toJson(it)}")
-                }
+                getLotIdsHandler.handle(node = request)
             }
         }
 
-        historyDao.saveHistory(operationId = id.toString(), command = action.toString(), response = response)
+        if (log.isDebugEnabled)
+            log.debug("DataOfResponse: '$response'.")
         return response
     }
 }
