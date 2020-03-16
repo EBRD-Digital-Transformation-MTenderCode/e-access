2da16d72b719b8b5553c2b41013a87a067004e14
yevhenii <yevheniiskoryi@gmail.com>
2020-03-16 18:24:29 +0200

    ES-2576 created request and application dtos for command 'checkAccessToTender'

---
+++ src/main/kotlin/com/procurement/access/application/service/tender/strategy/check/CheckAccessToTenderParams.kt
@@ -0,0 +1,73 @@
+package com.procurement.access.application.service.tender.strategy.check
+
+import com.procurement.access.domain.fail.error.DataErrors
+import com.procurement.access.domain.model.Cpid
+import com.procurement.access.domain.model.Ocid
+import com.procurement.access.domain.model.owner.Owner
+import com.procurement.access.domain.model.owner.tryCreateOwner
+import com.procurement.access.domain.model.token.Token
+import com.procurement.access.domain.model.token.tryCreateToken
+import com.procurement.access.domain.util.Result
+import com.procurement.access.domain.util.asSuccess
+
+class CheckAccessToTenderParams private constructor(
+    val cpid: Cpid,
+    val ocid: Ocid,
+    val owner: Owner,
+    val token: Token
+) {
+    companion object {
+        fun tryCreate(
+            cpid: String,
+            ocid: String,
+            owner: String,
+            token: String
+        ): Result<CheckAccessToTenderParams, DataErrors> {
+
+            val cpidResult = Cpid.tryCreate(cpid = cpid)
+                .doOnError { pattern ->
+                    DataErrors.Validation.DataMismatchToPattern(
+                        actualValue = cpid,
+                        name = "cpid",
+                        pattern = pattern
+                    )
+                }
+                .get
+            val ocidResult = Ocid.tryCreate(ocid = ocid)
+                .doOnError { pattern ->
+                    DataErrors.Validation.DataMismatchToPattern(
+                        actualValue = ocid,
+                        name = "ocid",
+                        pattern = pattern
+                    )
+                }
+                .get
+            val ownerResult = owner.tryCreateOwner()
+                .doOnError { pattern ->
+                    DataErrors.Validation.DataMismatchToPattern(
+                        actualValue = owner,
+                        name = "owner",
+                        pattern = pattern
+                    )
+                }
+                .get
+            val tokenResult = token.tryCreateToken()
+                .doOnError { pattern ->
+                    DataErrors.Validation.DataMismatchToPattern(
+                        actualValue = token,
+                        name = "token",
+                        pattern = pattern
+                    )
+                }
+                .get
+
+            return CheckAccessToTenderParams(
+                    cpid = cpidResult,
+                    ocid = ocidResult,
+                    owner = ownerResult,
+                    token = tokenResult
+                )
+                .asSuccess()
+        }
+    }
+}
