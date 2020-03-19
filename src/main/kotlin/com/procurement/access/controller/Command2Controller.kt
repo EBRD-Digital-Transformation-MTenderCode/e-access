962cc7a8a60fbc1ccb4b6a48753f30ab39684398
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-03-19 12:05:03 +0200

    ES-2656 added parsing version to Command2Controller

--- src/main/kotlin/com/procurement/access/controller/Command2Controller.kt
+++ src/main/kotlin/com/procurement/access/controller/Command2Controller.kt
@@ -1,11 +1,15 @@
 package com.procurement.access.controller
 
 import com.procurement.access.application.service.Logger
+import com.procurement.access.config.GlobalProperties
 import com.procurement.access.domain.fail.Fail
 import com.procurement.access.domain.fail.error.BadRequestErrors
 import com.procurement.access.infrastructure.web.dto.ApiResponse
+import com.procurement.access.infrastructure.web.dto.ApiVersion
+import com.procurement.access.model.dto.bpe.NaN
 import com.procurement.access.model.dto.bpe.errorResponse
 import com.procurement.access.model.dto.bpe.getId
+import com.procurement.access.model.dto.bpe.getVersion
 import com.procurement.access.service.CommandService2
 import com.procurement.access.utils.toJson
 import com.procurement.access.utils.toNode
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/command2")
@@ -28,7 +33,7 @@ class Command2Controller(
         @RequestBody requestBody: String
     ): ResponseEntity<ApiResponse> {
 
-            logger.info("RECEIVED COMMAND: '${requestBody}'.")
+        logger.info("RECEIVED COMMAND: '${requestBody}'.")
 
         val node = requestBody.toNode()
             .doOnError { error ->
@@ -41,21 +46,34 @@ class Command2Controller(
             }
             .get
 
+        val version = node.getVersion()
+            .doOnError { versionError ->
+                val id = node.getId()
+                    .doOnError { idError -> return responseEntity(expected = versionError) }
+                    .get
+                return responseEntity(expected = versionError, id = id)
+            }
+            .get
+
         val id = node.getId()
-            .doOnError { error -> return responseEntity(expected = error) }
+            .doOnError { error -> return responseEntity(expected = error, version = version) }
             .get
 
         val response = commandService2.execute(request = node)
             .also { response ->
-                    logger.info("RESPONSE (id: '${id}'): '${toJson(response)}'.")
+                logger.info("RESPONSE (id: '${id}'): '${toJson(response)}'.")
             }
 
         return ResponseEntity(response, HttpStatus.OK)
     }
 
-    private fun responseEntity(expected: Fail): ResponseEntity<ApiResponse> {
+    private fun responseEntity(
+        expected: Fail,
+        id: UUID = NaN,
+        version: ApiVersion = GlobalProperties.App.apiVersion
+    ): ResponseEntity<ApiResponse> {
         expected.logging(logger)
-        val response = errorResponse(fail = expected)
+        val response = errorResponse(fail = expected, id = id, version = version)
         return ResponseEntity(response, HttpStatus.OK)
     }
 }
