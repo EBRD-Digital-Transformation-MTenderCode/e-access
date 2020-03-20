7a5f0fa7b5a72ce6f75ba7cae4c1c1d05729dd1c
Evgen Kopach <zkopach@gmail.com>
2020-03-20 10:10:07 +0200

    ES-2634 Created converter for CheckPersonesStructureRequest

---
+++ src/main/kotlin/com/procurement/access/infrastructure/dto/converter/CheckPersonesStructureRequestConverter.kt
@@ -0,0 +1,84 @@
+package com.procurement.access.infrastructure.dto.converter
+
+import com.procurement.access.application.model.responder.check.structure.CheckPersonesStructureParams
+import com.procurement.access.domain.fail.error.DataErrors
+import com.procurement.access.domain.util.Result
+import com.procurement.access.domain.util.Result.Companion.failure
+import com.procurement.access.domain.util.extension.mapOptionalResult
+import com.procurement.access.domain.util.extension.mapResult
+import com.procurement.access.infrastructure.handler.check.persons.CheckPersonesStructureRequest
+
+fun CheckPersonesStructureRequest.convert(): Result<CheckPersonesStructureParams, DataErrors> {
+
+    val convertedPersons = this.persons
+        .mapResult { it.convert() }
+        .doOnError { error -> return failure(error) }
+        .get
+
+    return CheckPersonesStructureParams.tryCreate(
+        cpid = this.cpid,
+        ocid = this.ocid,
+        persons = convertedPersons,
+        locationOfPersones = this.locationOfPersones
+    )
+}
+
+private fun CheckPersonesStructureRequest.Person.convert(): Result<CheckPersonesStructureParams.Person, DataErrors> {
+    val identifier = this.identifier
+        .convert()
+        .doOnError { error -> return failure(error) }
+        .get
+
+    val businessFunctions = this.businessFunctions
+        .mapResult { it.convert() }
+        .doOnError { error -> return failure(error) }
+        .get
+
+    return CheckPersonesStructureParams.Person.tryCreate(
+        title = this.title,
+        name = this.name,
+        identifier = identifier,
+        businessFunctions = businessFunctions
+
+    )
+}
+
+private fun CheckPersonesStructureRequest.Person.BusinessFunction.convert(): Result<CheckPersonesStructureParams.Person.BusinessFunction, DataErrors> {
+    val period = this.period
+        .convert()
+        .doOnError { error -> return failure(error) }
+        .get
+
+    val documents = this.documents
+        .mapOptionalResult { it.convert() }
+        .doOnError { error -> return failure(error) }
+        .get
+
+    return CheckPersonesStructureParams.Person.BusinessFunction.tryCreate(
+        id = this.id,
+        jobTitle = this.jobTitle,
+        type = this.type,
+        period = period,
+        documents = documents
+    )
+}
+
+private fun CheckPersonesStructureRequest.Person.BusinessFunction.Document.convert(): Result<CheckPersonesStructureParams.Person.BusinessFunction.Document, DataErrors> =
+    CheckPersonesStructureParams.Person.BusinessFunction.Document.tryCreate(
+        id = this.id,
+        title = this.title,
+        description = this.description,
+        documentType = this.documentType
+    )
+
+private fun CheckPersonesStructureRequest.Person.BusinessFunction.Period.convert(): Result<CheckPersonesStructureParams.Person.BusinessFunction.Period, DataErrors> =
+    CheckPersonesStructureParams.Person.BusinessFunction.Period.tryCreate(
+        startDate = this.startDate
+    )
+
+private fun CheckPersonesStructureRequest.Person.Identifier.convert(): Result<CheckPersonesStructureParams.Person.Identifier, DataErrors> =
+    CheckPersonesStructureParams.Person.Identifier.tryCreate(
+        id = this.id,
+        scheme = this.scheme,
+        uri = this.uri
+    )
