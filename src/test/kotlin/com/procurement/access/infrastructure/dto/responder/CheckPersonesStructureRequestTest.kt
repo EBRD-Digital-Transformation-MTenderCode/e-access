121d59e1c9ed772fa07eeccf6d846c970a84d251
Evgen Kopach <zkopach@gmail.com>
2020-03-20 10:08:29 +0200

    ES-2634 Created tests from request dto for 'checkPersonesStructure' command

---
+++ src/test/kotlin/com/procurement/access/infrastructure/dto/responder/CheckPersonesStructureRequestTest.kt
@@ -0,0 +1,24 @@
+package com.procurement.access.infrastructure.dto.responder
+
+import com.procurement.access.infrastructure.AbstractDTOTestBase
+import com.procurement.access.infrastructure.handler.check.persons.CheckPersonesStructureRequest
+import org.junit.jupiter.api.Test
+
+class CheckPersonesStructureRequestTest : AbstractDTOTestBase<CheckPersonesStructureRequest>(
+    CheckPersonesStructureRequest::class.java
+) {
+    @Test
+    fun fully() {
+        testBindingAndMapping(pathToJsonFile = "json/dto/responder/check/structure/request/request_check_persones_structure_full.json")
+    }
+
+    @Test
+    fun required1() {
+        testBindingAndMapping(pathToJsonFile = "json/dto/responder/check/structure/request/request_check_persones_structure_required_1.json")
+    }
+
+    @Test
+    fun required2() {
+        testBindingAndMapping(pathToJsonFile = "json/dto/responder/check/structure/request/request_check_persones_structure_required_2.json")
+    }
+}
