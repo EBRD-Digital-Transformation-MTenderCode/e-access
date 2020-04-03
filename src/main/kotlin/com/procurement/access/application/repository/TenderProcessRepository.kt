be2b3e4fa150f810c7a67cdc8edcdb4b59b32336
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-04-03 15:46:29 +0300

    ES-2738 removed method 'findAuthByCpid' from 'TenderProcessRepository'

--- src/main/kotlin/com/procurement/access/application/repository/TenderProcessRepository.kt
+++ src/main/kotlin/com/procurement/access/application/repository/TenderProcessRepository.kt
@@ -4,16 +4,11 @@ import com.datastax.driver.core.ResultSet
 import com.procurement.access.domain.fail.Fail
 import com.procurement.access.domain.model.Cpid
 import com.procurement.access.domain.model.enums.Stage
-
 import com.procurement.access.domain.util.Result
 import com.procurement.access.model.entity.TenderProcessEntity
-import java.util.*
 
 interface TenderProcessRepository {
 
     fun save(entity: TenderProcessEntity): Result<ResultSet, Fail.Incident.Database>
     fun getByCpIdAndStage(cpid: Cpid, stage: Stage): Result<TenderProcessEntity?, Fail.Incident.Database>
-    fun findAuthByCpid(cpid: Cpid): Result<List<Auth>, Fail.Incident.Database>
 }
-
-class Auth(val token: UUID, val owner: String)
