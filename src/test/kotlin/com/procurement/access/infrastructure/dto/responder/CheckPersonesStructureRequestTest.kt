81a8461c125a634dc6d3d6318542da21eafeac78
katherine <kate.shaidurova@gmail.com>
2020-04-06 17:04:46 +0300

    ES-2816 Renamed persones to persons in json for CheckPersonesStructureRequestTest

--- src/test/kotlin/com/procurement/access/infrastructure/dto/responder/CheckPersonesStructureRequestTest.kt
+++ src/test/kotlin/com/procurement/access/infrastructure/dto/responder/CheckPersonesStructureRequestTest.kt
@@ -9,16 +9,16 @@ class CheckPersonesStructureRequestTest : AbstractDTOTestBase<CheckPersonesStruc
 ) {
     @Test
     fun fully() {
-        testBindingAndMapping(pathToJsonFile = "json/dto/responder/check/structure/request/request_check_persones_structure_full.json")
+        testBindingAndMapping(pathToJsonFile = "json/dto/responder/check/structure/request/request_check_persons_structure_full.json")
     }
 
     @Test
     fun required1() {
-        testBindingAndMapping(pathToJsonFile = "json/dto/responder/check/structure/request/request_check_persones_structure_required_1.json")
+        testBindingAndMapping(pathToJsonFile = "json/dto/responder/check/structure/request/request_check_persons_structure_required_1.json")
     }
 
     @Test
     fun required2() {
-        testBindingAndMapping(pathToJsonFile = "json/dto/responder/check/structure/request/request_check_persones_structure_required_2.json")
+        testBindingAndMapping(pathToJsonFile = "json/dto/responder/check/structure/request/request_check_persons_structure_required_2.json")
     }
 }
