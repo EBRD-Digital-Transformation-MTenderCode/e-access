06e2948049c69f3582c4b72279408c7fa6dc632c
katherine <kate.shaidurova@gmail.com>
2020-04-07 12:01:59 +0300

    ES-2814 Added CHECK_PERSONS_STRUCTURE branch to CommandService2

--- src/main/kotlin/com/procurement/access/model/dto/bpe/Command2Message.kt
+++ src/main/kotlin/com/procurement/access/model/dto/bpe/Command2Message.kt
@@ -33,7 +33,8 @@ enum class Command2Type(@JsonValue override val key: String) : EnumElementProvid
     CHECK_ACCESS_TO_TENDER("checkAccessToTender"),
     GET_LOT_STATE_BY_IDS("getLotStateByIds"),
     RESPONDER_PROCESSING("responderProcessing"),
-    CHECK_PERSONS_STRUCTURE("checkPersonsStructure");
+    CHECK_PERSONS_STRUCTURE("checkPersonsStructure"),
+    GET_TENDER_STATE("getTenderState");
 
     override fun toString(): String = key
 
