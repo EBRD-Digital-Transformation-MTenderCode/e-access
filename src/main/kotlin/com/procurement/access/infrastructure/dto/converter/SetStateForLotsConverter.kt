c247740d53d693c41cc4c78811096977761781a6
katherine <kate.shaidurova@gmail.com>
2020-04-23 11:12:50 +0300

    ES-3019 Added convertToSetStateForLotsResult to SetStateForLotsConverter.kt

--- src/main/kotlin/com/procurement/access/infrastructure/dto/converter/SetStateForLotsConverter.kt
+++ src/main/kotlin/com/procurement/access/infrastructure/dto/converter/SetStateForLotsConverter.kt
@@ -1,12 +1,17 @@
 package com.procurement.access.infrastructure.dto.converter
 
 import com.procurement.access.application.model.params.SetStateForLotsParams
+import com.procurement.access.domain.fail.Fail
 import com.procurement.access.domain.fail.error.DataErrors
+import com.procurement.access.domain.model.lot.tryCreateLotId
 import com.procurement.access.domain.util.Result
 import com.procurement.access.domain.util.asFailure
+import com.procurement.access.domain.util.asSuccess
 import com.procurement.access.domain.util.extension.mapResult
 import com.procurement.access.domain.util.extension.toSetBy
+import com.procurement.access.infrastructure.entity.CNEntity
 import com.procurement.access.infrastructure.handler.set.stateforlots.SetStateForLotsRequest
+import com.procurement.access.infrastructure.handler.set.stateforlots.SetStateForLotsResult
 
 fun SetStateForLotsRequest.convert(): Result<SetStateForLotsParams, DataErrors> {
 
@@ -39,3 +44,11 @@ fun SetStateForLotsRequest.convert(): Result<SetStateForLotsParams, DataErrors> 
 fun SetStateForLotsRequest.Lot.convert(): Result<SetStateForLotsParams.Lot, DataErrors> {
     return SetStateForLotsParams.Lot.tryCreate(id = this.id, status = this.status, statusDetails = this.statusDetails)
 }
+
+fun CNEntity.Tender.Lot.convertToSetStateForLotsResult():Result<SetStateForLotsResult, Fail.Incident.Parsing> =
+    SetStateForLotsResult(
+        id = id.tryCreateLotId()
+            .forwardResult { fail -> return fail },
+        status = status,
+        statusDetails = statusDetails
+    ).asSuccess()
