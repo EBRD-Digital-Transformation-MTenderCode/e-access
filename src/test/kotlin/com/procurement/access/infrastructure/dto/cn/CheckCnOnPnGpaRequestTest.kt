d63bb0b34352950882b3dd40a6bc31dd4d951588
Evgen Kopach <zkopach@gmail.com>
2020-04-30 11:51:27 +0300

    ES-3125 Renamed json test files for request dto at 'checkCnOnPn' with pmd=GPA

--- src/test/kotlin/com/procurement/access/infrastructure/dto/cn/CheckCnOnPnGpaRequestTest.kt
+++ src/test/kotlin/com/procurement/access/infrastructure/dto/cn/CheckCnOnPnGpaRequestTest.kt
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Test
 class CheckCnOnPnGpaRequestTest : AbstractDTOTestBase<CheckCnOnPnGpaRequest>(CheckCnOnPnGpaRequest::class.java) {
     @Test
     fun fully() {
-        testBindingAndMapping("json/dto/create/cn_on_pn/gpa/check/request/request_cn_on_pn_gpa_full.json")
+        testBindingAndMapping("json/dto/create/cn_on_pn/gpa/check/request/request_check_cn_on_pn_gpa_full.json")
     }
 
     @Test
     fun required_1() {
-        testBindingAndMapping("json/dto/create/cn_on_pn/gpa/check/request/request_cn_on_pn_gpa_required_1.json")
+        testBindingAndMapping("json/dto/create/cn_on_pn/gpa/check/request/request_check_cn_on_pn_gpa_required_1.json")
     }
 
     @Test
     fun required_2() {
-        testBindingAndMapping("json/dto/create/cn_on_pn/gpa/check/request/request_cn_on_pn_gpa_required_2.json")
+        testBindingAndMapping("json/dto/create/cn_on_pn/gpa/check/request/request_check_cn_on_pn_gpa_required_2.json")
     }
 }
