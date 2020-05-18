3f61cafca66d8bbc692fbf629f56888afed5a7d4
Evgen Kopach <zkopach@gmail.com>
2020-05-18 16:05:20 +0300

    ES-3259 Created RequirementId.kt datatype

---
+++ src/main/kotlin/com/procurement/access/domain/model/requirement/RequirementId.kt
@@ -0,0 +1,19 @@
+package com.procurement.access.domain.model.requirement
+
+import com.procurement.access.domain.fail.error.DataErrors
+import com.procurement.access.domain.util.Result
+import java.util.*
+
+typealias RequirementId = UUID
+
+fun String.tryToRequirementId(): Result<RequirementId, DataErrors> = try {
+    Result.success(RequirementId.fromString(this))
+} catch (expected: Exception) {
+    Result.failure(
+        DataErrors.Validation.DataFormatMismatch(
+            name = "requirementId",
+            expectedFormat = "uuid",
+            actualValue = this
+        )
+    )
+}
