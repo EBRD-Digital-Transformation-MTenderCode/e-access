9a10820a00f07a38fa9c786dd1d05d153585535b
Evgen Kopach <zkopach@gmail.com>
2020-06-02 18:20:31 +0300

    ES-3401 Created tests for response DTO at 'createCriteriaForProcuringEntity' command

---
+++ src/test/kotlin/com/procurement/access/infrastructure/dto/criteria/CreateCriteriaForProcuringEntityResultTest.kt
@@ -0,0 +1,19 @@
+package com.procurement.access.infrastructure.dto.criteria
+
+import com.procurement.access.infrastructure.AbstractDTOTestBase
+import com.procurement.access.infrastructure.handler.create.CreateCriteriaForProcuringEntityResult
+import org.junit.jupiter.api.Test
+
+class CreateCriteriaForProcuringEntityResultTest : AbstractDTOTestBase<CreateCriteriaForProcuringEntityResult>(
+    CreateCriteriaForProcuringEntityResult::class.java
+) {
+    @Test
+    fun fully() {
+        testBindingAndMapping(pathToJsonFile = "json/dto/tender/get/criteria/result_create_criteria_for_procuring_entity_full.json")
+    }
+
+    @Test
+    fun required1() {
+        testBindingAndMapping(pathToJsonFile = "json/dto/tender/get/criteria/result_create_criteria_for_procuring_entity_required_1.json")
+    }
+}
