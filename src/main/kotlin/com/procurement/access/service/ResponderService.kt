9dbec07cb239d8b40b61ebc7b3c04c7da38021ba
yevhenii <yevheniiskoryi@gmail.com>
2020-07-01 14:53:03 +0300

    ES-3652 added request dto with mapping tests for command 'checkTenderState'

--- src/main/kotlin/com/procurement/access/service/ResponderService.kt
+++ src/main/kotlin/com/procurement/access/service/ResponderService.kt
@@ -224,7 +224,9 @@ class ResponderServiceImpl(
             OperationType.CREATE_CN_ON_PN,
             OperationType.CREATE_CN_ON_PIN,
             OperationType.CREATE_PIN_ON_PN,
-            OperationType.CREATE_NEGOTIATION_CN_ON_PN -> emptyMap()
+            OperationType.CREATE_NEGOTIATION_CN_ON_PN,
+            OperationType.QUALIFICATION,
+            OperationType.QUALIFICATION_CONSIDERATION -> emptyMap()
         }
 
         val organizationIdsSet = params.organizationIds.toSet()
