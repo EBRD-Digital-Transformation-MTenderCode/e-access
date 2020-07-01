b1bcd02e3f1fdd4b75e0d521975f496200f5184f
yevhenii <yevheniiskoryi@gmail.com>
2020-07-01 14:52:05 +0300

    ES-3652 added 'validStates' parameter to 'access_rules' table

--- src/main/kotlin/com/procurement/access/service/RulesService.kt
+++ src/main/kotlin/com/procurement/access/service/RulesService.kt
@@ -1,22 +1,75 @@
 package com.procurement.access.service
 
 import com.procurement.access.dao.RulesDao
+import com.procurement.access.domain.fail.Fail
+import com.procurement.access.domain.fail.error.ValidationErrors
 import com.procurement.access.domain.model.enums.MainProcurementCategory
+import com.procurement.access.domain.model.enums.OperationType
 import com.procurement.access.domain.model.enums.ProcurementMethod
+import com.procurement.access.domain.util.Result
+import com.procurement.access.domain.util.asFailure
+import com.procurement.access.domain.util.asSuccess
 import com.procurement.access.exception.ErrorException
 import com.procurement.access.exception.ErrorType
+import com.procurement.access.model.dto.state.States
+import com.procurement.access.utils.tryToObject
 import org.springframework.stereotype.Service
 
 @Service
 class RulesService(private val rulesDao: RulesDao) {
 
+    companion object {
+        private const val VALID_STATES_PARAMETER = "validStates"
+        private const val OPERATION_TYPE_ALL = "all"
+    }
+
     fun isAuctionRequired(
         country: String,
         pmd: ProcurementMethod,
         mainProcurementCategory: MainProcurementCategory
-    ): Boolean = getValue(country, pmd, mainProcurementCategory.key).toBoolean()
+    ): Boolean = getValue(country, pmd, OPERATION_TYPE_ALL, mainProcurementCategory.key).toBoolean()
 
-    private fun getValue(country: String, pmd: ProcurementMethod, parameter: String): String =
-        rulesDao.getValue(country, pmd, parameter)
+    fun getTenderStates(
+        country: String,
+        pmd: ProcurementMethod,
+        operationType: OperationType
+    ): Result<States, Fail> =
+        getData(country = country, operationType = operationType, pmd = pmd, parameter = VALID_STATES_PARAMETER)
+
+    private fun getValue(
+        country: String,
+        pmd: ProcurementMethod,
+        operationType: String,
+        parameter: String
+    ): String =
+        rulesDao.getValue(country, pmd, operationType, parameter)
             ?: throw ErrorException(ErrorType.RULES_NOT_FOUND)
+
+    private fun getData(
+        country: String,
+        pmd: ProcurementMethod,
+        operationType: OperationType,
+        parameter: String
+    ): Result<States, Fail> {
+        val states = rulesDao.getData(
+            country = country,
+            pmd = pmd,
+            operationType = operationType,
+            parameter = parameter
+        )
+            .orForwardFail { fail -> return fail }
+            ?: return ValidationErrors.TenderStatesNotFound(pmd = pmd, operationType = operationType, country = country)
+                .asFailure()
+
+        return states.convert()
+            .orForwardFail { fail -> return fail }
+            .asSuccess()
+    }
+
+    private fun String.convert(): Result<States, Fail> =
+        this.tryToObject(States::class.java)
+            .doReturn { error ->
+                return Result.failure(Fail.Incident.DatabaseIncident(exception = error.exception))
+            }
+            .asSuccess()
 }
