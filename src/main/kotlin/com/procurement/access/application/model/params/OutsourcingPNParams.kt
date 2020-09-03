0cf26fca7622c649b393389257f2d0ab8f8cf75e
Evgen Kopach <zkopach@gmail.com>
2020-09-03 13:06:24 +0300

    ES-4132 Created params for request DTO for 'outsourcingPN' command.

---
+++ src/main/kotlin/com/procurement/access/application/model/params/OutsourcingPNParams.kt
@@ -0,0 +1,28 @@
+package com.procurement.access.application.model.params
+
+import com.procurement.access.application.model.parseCpid
+import com.procurement.access.application.model.parseOcid
+import com.procurement.access.domain.fail.error.DataErrors
+import com.procurement.access.domain.model.Cpid
+import com.procurement.access.domain.model.Ocid
+import com.procurement.access.domain.util.Result
+import com.procurement.access.domain.util.Result.Companion.success
+
+class OutsourcingPNParams(
+    val cpid: Cpid,
+    val ocid: Ocid,
+    val cpidFA: String
+) {
+
+    companion object {
+        fun tryCreate(cpid: String, ocid: String, cpidFA: String): Result<OutsourcingPNParams, DataErrors> {
+            val cpidResult = parseCpid(value = cpid)
+                .orForwardFail { error -> return error }
+
+            val ocidResult = parseOcid(value = ocid)
+                .orForwardFail { error -> return error }
+
+            return success(OutsourcingPNParams(cpid = cpidResult, ocid = ocidResult, cpidFA = cpidFA))
+        }
+    }
+}
