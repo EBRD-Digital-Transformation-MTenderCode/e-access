84181be91c0a8055abb36dc69817eb5c57335d9c
Evgen Kopach <zkopach@gmail.com>
2020-09-15 12:21:09 +0300

    ES-4210 Renamed test files.

--- src/test/kotlin/com/procurement/access/infrastructure/dto/ap/CreateApResponseTest.kt
+++ src/test/kotlin/com/procurement/access/infrastructure/dto/ap/CreateApResponseTest.kt
@@ -3,19 +3,19 @@ package com.procurement.access.infrastructure.dto.ap
 import com.procurement.access.infrastructure.AbstractDTOTestBase
 import org.junit.jupiter.api.Test
 
-class CreateApResponseTest : AbstractDTOTestBase<ApCreateRequest>(ApCreateRequest::class.java) {
+class CreateApResponseTest : AbstractDTOTestBase<ApCreateResponse>(ApCreateResponse::class.java) {
     @Test
     fun fully() {
-        testBindingAndMapping("json/dto/ap/create/request/request_create_ap_full.json")
+        testBindingAndMapping("json/dto/ap/create/response/response_create_ap_full.json")
     }
 
     @Test
     fun required1() {
-        testBindingAndMapping("json/dto/ap/create/request/request_create_ap_required_1.json")
+        testBindingAndMapping("json/dto/ap/create/response/response_create_ap_required_1.json")
     }
 
     @Test
     fun required2() {
-        testBindingAndMapping("json/dto/ap/create/request/request_create_ap_required_2.json")
+        testBindingAndMapping("json/dto/ap/create/response/response_create_ap_required_2.json")
     }
 }
