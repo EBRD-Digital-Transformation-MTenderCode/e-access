ca5002f75712b3b9b91996ef356e8ae5f0b21b8a
Evgen Kopach <zkopach@gmail.com>
2020-09-16 12:37:35 +0300

    ES-4210 Created request DTO for 'updateAp' command.

---
+++ src/test/kotlin/com/procurement/access/infrastructure/dto/ap/update/UpdateApRequestTest.kt
@@ -0,0 +1,32 @@
+package com.procurement.access.infrastructure.dto.ap.update
+
+import com.procurement.access.infrastructure.AbstractDTOTestBase
+import org.junit.jupiter.api.Test
+
+class UpdateApRequestTest : AbstractDTOTestBase<ApUpdateRequest>(ApUpdateRequest::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping("json/dto/ap/update/request/request_update_ap_full.json")
+    }
+
+    @Test
+    fun required1() {
+        testBindingAndMapping("json/dto/ap/update/request/request_update_ap_required_1.json")
+    }
+
+    @Test
+    fun required2() {
+        testBindingAndMapping("json/dto/ap/update/request/request_update_ap_required_2.json")
+    }
+
+    @Test
+    fun required3() {
+        testBindingAndMapping("json/dto/ap/update/request/request_update_ap_required_3.json")
+    }
+
+    @Test
+    fun required4() {
+        testBindingAndMapping("json/dto/ap/update/request/request_update_ap_required_4.json")
+    }
+}
