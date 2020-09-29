430f313464d51b6a9e5bd53c4ebd244c0cc936fb
katherine <kate.shaidurova@gmail.com>
2020-09-29 15:57:38 +0300

    ES-4328 Returned documentType parameter its DocumentType enum type in CheckFEDataRequest.kt, refactored documentType affiliation to allowed elements check

--- src/main/kotlin/com/procurement/access/infrastructure/dto/fe/check/converter/CheckFEDataRequestConverter.kt
+++ src/main/kotlin/com/procurement/access/infrastructure/dto/fe/check/converter/CheckFEDataRequestConverter.kt
@@ -2,8 +2,6 @@ package com.procurement.access.infrastructure.dto.fe.check.converter
 
 import com.procurement.access.application.service.fe.check.CheckFEDataData
 import com.procurement.access.domain.model.enums.BusinessFunctionType
-import com.procurement.access.domain.EnumElementProviderParser
-import com.procurement.access.domain.model.enums.DocumentType
 import com.procurement.access.exception.ErrorException
 import com.procurement.access.exception.ErrorType
 import com.procurement.access.infrastructure.dto.fe.check.CheckFEDataRequest
@@ -52,48 +50,8 @@ fun CheckFEDataRequest.Tender.Document.convert() = CheckFEDataData.Tender.Docume
     id = this.id,
     title = this.title,
     description = this.description,
-    documentType = checkAndGetDocumentType(this.documentType)
+    documentType = this.documentType
 )
-
-private fun checkAndGetDocumentType(documentType: String): DocumentType {
-    return EnumElementProviderParser.checkAndParseEnum(
-        value = documentType,
-        allowedValues = allowedTenderDocumentTypes,
-        target = DocumentType
-    )
-}
-
-private val allowedTenderDocumentTypes = DocumentType.allowedElements
-    .filter {
-        when (it) {
-            DocumentType.TENDER_NOTICE,
-            DocumentType.BIDDING_DOCUMENTS,
-            DocumentType.TECHNICAL_SPECIFICATIONS,
-            DocumentType.EVALUATION_CRITERIA,
-            DocumentType.CLARIFICATIONS,
-            DocumentType.ELIGIBILITY_CRITERIA,
-            DocumentType.RISK_PROVISIONS,
-            DocumentType.BILL_OF_QUANTITY,
-            DocumentType.CONFLICT_OF_INTEREST,
-            DocumentType.PROCUREMENT_PLAN,
-            DocumentType.CONTRACT_DRAFT,
-            DocumentType.COMPLAINTS,
-            DocumentType.ILLUSTRATION,
-            DocumentType.CANCELLATION_DETAILS,
-            DocumentType.EVALUATION_REPORTS,
-            DocumentType.SHORTLISTED_FIRMS,
-            DocumentType.CONTRACT_ARRANGEMENTS,
-            DocumentType.CONTRACT_GUARANTEES -> true
-
-            DocumentType.ASSET_AND_LIABILITY_ASSESSMENT,
-            DocumentType.ENVIRONMENTAL_IMPACT,
-            DocumentType.FEASIBILITY_STUDY,
-            DocumentType.HEARING_NOTICE,
-            DocumentType.MARKET_STUDIES,
-            DocumentType.NEEDS_ASSESSMENT,
-            DocumentType.PROJECT_PLAN -> false
-        }
-    }.toSet()
 
 fun CheckFEDataRequest.Tender.Criteria.convert() = CheckFEDataData.Tender.Criteria(
     id = this.id,
