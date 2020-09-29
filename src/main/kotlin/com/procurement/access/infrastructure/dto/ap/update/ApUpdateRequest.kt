9d4193741aab8fc5bfd78d741bc03f1b8ab16d73
katherine <kate.shaidurova@gmail.com>
2020-09-29 13:14:13 +0300

    ES-4299 Made streetAddress optional for ApUpdate request, data, response and entity

--- src/main/kotlin/com/procurement/access/infrastructure/dto/ap/update/ApUpdateRequest.kt
+++ src/main/kotlin/com/procurement/access/infrastructure/dto/ap/update/ApUpdateRequest.kt
@@ -214,7 +214,8 @@ class ApUpdateRequest(
         }
 
         data class Address(
-            @field:JsonProperty("streetAddress") @param:JsonProperty("streetAddress") val streetAddress: String,
+            @JsonInclude(JsonInclude.Include.NON_NULL)
+            @field:JsonProperty("streetAddress") @param:JsonProperty("streetAddress") val streetAddress: String?,
 
             @JsonInclude(JsonInclude.Include.NON_NULL)
             @field:JsonProperty("postalCode") @param:JsonProperty("postalCode") val postalCode: String?,
