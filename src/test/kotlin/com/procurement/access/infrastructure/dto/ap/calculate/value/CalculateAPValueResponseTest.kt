05582e5b618a546d37b83b6b34db0dc6e126bf46
Yevhen Kopach <zkopach@gmail.com>
2020-10-07 10:28:35 +0300

    ES-4409 Create response DTO. Added tests mapping tests.

---
+++ src/test/kotlin/com/procurement/access/infrastructure/dto/ap/calculate/value/CalculateAPValueResponseTest.kt
@@ -0,0 +1,13 @@
+package com.procurement.access.infrastructure.dto.ap.calculate.value
+
+import com.procurement.access.infrastructure.AbstractDTOTestBase
+import com.procurement.access.infrastructure.handler.calculate.value.CalculateAPValueResult
+import org.junit.jupiter.api.Test
+
+class CalculateAPValueResponseTest : AbstractDTOTestBase<CalculateAPValueResult>(CalculateAPValueResult::class.java) {
+
+    @Test
+    fun fully() {
+        testBindingAndMapping(pathToJsonFile = "json/dto/ap/calculate/value/response/calculate_ap_value_response_full.json")
+    }
+}
