77546d257bbf94c60b7b6fb6c3e33674b2bd3ced
katherine <kate.shaidurova@gmail.com>
2020-10-15 18:09:59 +0300

    ES-4482 Added validateClassification() tests

--- src/test/kotlin/com/procurement/access/service/validation/ValidationServiceTest.kt
+++ src/test/kotlin/com/procurement/access/service/validation/ValidationServiceTest.kt
@@ -3,6 +3,7 @@ package com.procurement.access.service.validation
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
 import com.procurement.access.application.model.params.CheckEqualPNAndAPCurrencyParams
+import com.procurement.access.application.model.params.ValidateClassificationParams
 import com.procurement.access.application.repository.TenderProcessRepository
 import com.procurement.access.dao.TenderProcessDao
 import com.procurement.access.domain.model.Cpid
@@ -17,6 +18,7 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import java.util.*
 
 class ValidationServiceTest {
 
@@ -30,7 +32,8 @@ class ValidationServiceTest {
         private val OCID = Ocid.tryCreateOrNull("ocds-b3wdp1-MD-1581509539187-EV-1581509653044")!!
         private val CPID_AP = Cpid.tryCreateOrNull("ocds-t1s2t3-OT-1565251033097")!!
         private val OCID_AP = Ocid.tryCreateOrNull("ocds-b3wdp1-MD-1581509539187-FE-1581509653045")!!
-
+        private val VALID_CLASSIFICATION_ID = "7a6cd0c8-006d-474b-8ab3-7a2a6cb9aa22"
+        private val STORED_CLASSIFICATION_ID = "7a6b670f-858b-4bed-8f19-dc5886b03ca4"
 
     }
 
@@ -71,7 +74,7 @@ class ValidationServiceTest {
             whenever(tenderProcessRepository.getByCpIdAndStage(cpid = params.cpid, stage = params.ocid.stage))
                 .thenReturn(success(null))
 
-            val actual =  validationService.checkEqualPNAndAPCurrency(params = getParams()).error
+            val actual =  validationService.checkEqualPNAndAPCurrency(params).error
 
             val expectedErrorCode = "VR.COM-1.33.1"
             val expectedErrorMessage = "Tender not found by cpid='${params.cpid}' and ocid='${params.ocid}'."
@@ -131,4 +134,66 @@ class ValidationServiceTest {
             ocidAP = OCID_AP.toString()
         ).get
     }
+
+    @Nested
+    inner class ValidateClassification{
+
+        @Test
+        fun recordNotFound_fail(){
+            val params = getParams(UUID.randomUUID().toString())
+
+            whenever(tenderProcessRepository.getByCpIdAndStage(cpid = params.cpid, stage = params.ocid.stage))
+                .thenReturn(success(null))
+
+            val actual =  validationService.validateClassification(params).error
+
+            val expectedErrorCode = "VR.COM-1.30.1"
+            val expectedErrorMessage = "Tender not found by cpid='${params.cpid}' and ocid='${params.ocid}'."
+
+            assertEquals(expectedErrorCode, actual.code)
+            assertEquals(expectedErrorMessage, actual.description)
+        }
+
+        @Test
+        fun classificationDoesNotMatch_fail(){
+            val params = getParams(UUID.randomUUID().toString())
+
+            val json = loadJson("json/service/validate/classification/tender_classification_entity.json")
+            val tenderProcessEntity = TenderProcessEntityGenerator.generate(data = json)
+
+            whenever(tenderProcessRepository.getByCpIdAndStage(cpid = params.cpid, stage = params.ocid.stage))
+                .thenReturn(success(tenderProcessEntity))
+
+            val actual =  validationService.validateClassification(params).error
+
+            val expectedErrorCode = "VR.COM-1.30.2"
+            val expectedErrorMessage = "First three symbols of received classification id '${params.tender.classification.id}' does not match stored one '$STORED_CLASSIFICATION_ID'."
+
+            assertEquals(expectedErrorCode, actual.code)
+            assertEquals(expectedErrorMessage, actual.description)
+        }
+
+        @Test
+        fun classificationsMatch_success(){
+            val params = getParams(VALID_CLASSIFICATION_ID)
+
+            val json = loadJson("json/service/validate/classification/tender_classification_entity.json")
+            val tenderProcessEntity = TenderProcessEntityGenerator.generate(data = json)
+
+            whenever(tenderProcessRepository.getByCpIdAndStage(cpid = params.cpid, stage = params.ocid.stage))
+                .thenReturn(success(tenderProcessEntity))
+
+            val actual =  validationService.validateClassification(params)
+
+            assertTrue(actual is ValidationResult.Ok)
+        }
+
+        private fun getParams(classificationId: String) = ValidateClassificationParams.tryCreate(
+            cpid = CPID.toString(),
+            ocid = OCID.toString(),
+            tender = ValidateClassificationParams.Tender(ValidateClassificationParams.Tender.Classification(id = classificationId))
+        ).get
+    }
+
+
 }
