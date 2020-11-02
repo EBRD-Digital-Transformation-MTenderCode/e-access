e84fa98ca1f35496578382cd54c2bbc504781723
katherine <kate.shaidurova@gmail.com>
2020-11-02 18:04:08 +0200

    ES-4545 Moved documentType check from service to converter and added its parsing

--- src/main/kotlin/com/procurement/access/application/service/command/CheckFEDataRules.kt
+++ src/main/kotlin/com/procurement/access/application/service/command/CheckFEDataRules.kt
@@ -4,7 +4,6 @@ import com.procurement.access.application.model.context.CheckFEDataContext
 import com.procurement.access.application.service.fe.check.CheckFEDataData
 import com.procurement.access.dao.TenderProcessDao
 import com.procurement.access.domain.model.enums.CriteriaRelatesToEnum
-import com.procurement.access.domain.model.enums.DocumentType
 import com.procurement.access.domain.model.enums.OperationType
 import com.procurement.access.domain.model.enums.RequirementDataType
 import com.procurement.access.exception.ErrorException
@@ -25,38 +24,6 @@ import java.time.LocalDateTime
 
 class CheckFEDataRules {
     companion object {
-
-        private val allowedTenderDocumentTypes = DocumentType.allowedElements
-            .filter {
-                when (it) {
-                    DocumentType.TENDER_NOTICE,
-                    DocumentType.BIDDING_DOCUMENTS,
-                    DocumentType.TECHNICAL_SPECIFICATIONS,
-                    DocumentType.EVALUATION_CRITERIA,
-                    DocumentType.CLARIFICATIONS,
-                    DocumentType.ELIGIBILITY_CRITERIA,
-                    DocumentType.RISK_PROVISIONS,
-                    DocumentType.BILL_OF_QUANTITY,
-                    DocumentType.CONFLICT_OF_INTEREST,
-                    DocumentType.PROCUREMENT_PLAN,
-                    DocumentType.CONTRACT_DRAFT,
-                    DocumentType.COMPLAINTS,
-                    DocumentType.ILLUSTRATION,
-                    DocumentType.CANCELLATION_DETAILS,
-                    DocumentType.EVALUATION_REPORTS,
-                    DocumentType.SHORTLISTED_FIRMS,
-                    DocumentType.CONTRACT_ARRANGEMENTS,
-                    DocumentType.CONTRACT_GUARANTEES -> true
-
-                    DocumentType.ASSET_AND_LIABILITY_ASSESSMENT,
-                    DocumentType.ENVIRONMENTAL_IMPACT,
-                    DocumentType.FEASIBILITY_STUDY,
-                    DocumentType.HEARING_NOTICE,
-                    DocumentType.MARKET_STUDIES,
-                    DocumentType.NEEDS_ASSESSMENT,
-                    DocumentType.PROJECT_PLAN -> false
-                }
-            }.toSet()
 
         fun validateTitle(title: String) {
             // VR-1.0.1.1.7
@@ -94,17 +61,6 @@ class CheckFEDataRules {
                     error = ErrorType.NOT_UNIQUE_IDS,
                     message = "$collectionName contains duplicated ids."
                 )
-        }
-
-        fun validateDocumentsTypes(data: CheckFEDataData) {
-            data.tender.documents
-                .map { document ->
-                    if (document.documentType !in allowedTenderDocumentTypes)
-                        throw ErrorException(
-                            error = ErrorType.INCORRECT_VALUE_ATTRIBUTE,
-                            message = "Tender document '${document.id}' contains incorrect documentType '${document.documentType}'. Allowed values: '${allowedTenderDocumentTypes.joinToString()}'"
-                        )
-                }
         }
 
         fun validatePersonsExistence(persons: List<CheckFEDataData.Tender.ProcuringEntity.Person>) {
