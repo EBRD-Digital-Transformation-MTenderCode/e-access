06c3773ba37d8231c658ce2007e356db079b694e
katherine <kate.shaidurova@gmail.com>
2020-11-02 17:39:22 +0200

    ES-4516 Made documentType String in request

--- src/main/kotlin/com/procurement/access/infrastructure/dto/ap/update/ApUpdateRequest.kt
+++ src/main/kotlin/com/procurement/access/infrastructure/dto/ap/update/ApUpdateRequest.kt
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.procurement.access.domain.model.CPVCode
-import com.procurement.access.domain.model.enums.DocumentType
 import com.procurement.access.domain.model.enums.MainProcurementCategory
 import com.procurement.access.domain.model.enums.Scheme
 import com.procurement.access.infrastructure.bind.quantity.QuantityDeserializer
@@ -246,7 +245,7 @@ class ApUpdateRequest(
 
         data class Document(
             @field:JsonProperty("id") @param:JsonProperty("id") val id: String,
-            @field:JsonProperty("documentType") @param:JsonProperty("documentType") val documentType: DocumentType,
+            @field:JsonProperty("documentType") @param:JsonProperty("documentType") val documentType: String,
             @field:JsonProperty("title") @param:JsonProperty("title") val title: String,
 
             @JsonInclude(JsonInclude.Include.NON_NULL)
