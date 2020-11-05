f500b282ed12256fcb76300e737f0bdadccb909b
Evgen Kopach <zkopach@gmail.com>
2020-11-05 15:52:15 +0200

    ES-4459 Updated FEEntity.kt. Added default value to persones array. Added NON_EMPTY annotation.

--- src/main/kotlin/com/procurement/access/infrastructure/entity/FEEntity.kt
+++ src/main/kotlin/com/procurement/access/infrastructure/entity/FEEntity.kt
@@ -177,12 +177,16 @@ data class FEEntity(
 
         data class ProcuringEntity(
             @field:JsonProperty("id") @param:JsonProperty("id") val id: String,
-            @field:JsonProperty("persones") @param:JsonProperty("persones") val persons: List<Person>,
+
+            @JsonInclude(JsonInclude.Include.NON_EMPTY)
+            @field:JsonProperty("persones") @param:JsonProperty("persones") val persons: List<Person> = emptyList(),
+
             @field:JsonProperty("name") @param:JsonProperty("name") val name: String,
             @field:JsonProperty("identifier") @param:JsonProperty("identifier") val identifier: Identifier,
 
             @JsonInclude(JsonInclude.Include.NON_EMPTY)
             @field:JsonProperty("additionalIdentifiers") @param:JsonProperty("additionalIdentifiers") val additionalIdentifiers: List<Identifier>?,
+
             @field:JsonProperty("address") @param:JsonProperty("address") val address: Address,
             @field:JsonProperty("contactPoint") @param:JsonProperty("contactPoint") val contactPoint: ContactPoint
         ) {
