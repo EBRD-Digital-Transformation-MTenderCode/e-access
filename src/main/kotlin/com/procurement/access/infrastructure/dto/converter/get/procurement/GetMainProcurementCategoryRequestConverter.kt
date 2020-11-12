e91f2382fc1e2b42510d16289ce12120d8a87be2
katherine <kate.shaidurova@gmail.com>
2020-11-12 18:58:37 +0200

    ES-4819 Added converter

---
+++ src/main/kotlin/com/procurement/access/infrastructure/dto/converter/get/procurement/GetMainProcurementCategoryRequestConverter.kt
@@ -0,0 +1,23 @@
+package com.procurement.access.infrastructure.dto.converter.get.procurement
+
+import com.procurement.access.application.model.params.GetMainProcurementCategoryParams
+import com.procurement.access.application.model.parseCpid
+import com.procurement.access.application.model.parseOcid
+import com.procurement.access.domain.fail.error.DataErrors
+import com.procurement.access.domain.util.Result
+import com.procurement.access.domain.util.asSuccess
+import com.procurement.access.infrastructure.handler.get.procurement.GetMainProcurementCategoryRequest
+
+fun GetMainProcurementCategoryRequest.convert(): Result<GetMainProcurementCategoryParams, DataErrors> {
+    val cpidParsed = parseCpid(cpid)
+        .orForwardFail { return it }
+
+    val ocidParsed = parseOcid(ocid)
+        .orForwardFail { return it }
+
+    return GetMainProcurementCategoryParams(
+        cpid = cpidParsed, ocid = ocidParsed
+    ).asSuccess()
+}
+
+
