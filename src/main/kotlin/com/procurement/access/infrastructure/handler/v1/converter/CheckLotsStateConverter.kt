d1c14034950a7dcf62684effa4542106ba319c0b
katherine <kate.shaidurova@gmail.com>
2020-11-30 15:58:05 +0200

    ES-4949 Added dtos and converter

---
+++ src/main/kotlin/com/procurement/access/infrastructure/handler/v1/converter/CheckLotsStateConverter.kt
@@ -0,0 +1,96 @@
+package com.procurement.access.infrastructure.handler.v1.converter
+
+import com.procurement.access.application.model.params.CheckLotsStateParams
+import com.procurement.access.application.model.parseCpid
+import com.procurement.access.application.model.parseLotId
+import com.procurement.access.application.model.parseOcid
+import com.procurement.access.application.model.parseOperationType
+import com.procurement.access.application.model.parsePmd
+import com.procurement.access.domain.fail.error.DataErrors
+import com.procurement.access.domain.model.enums.OperationType
+import com.procurement.access.domain.model.enums.ProcurementMethod
+import com.procurement.access.infrastructure.handler.v2.model.request.CheckLotsStateRequest
+import com.procurement.access.lib.extension.mapResult
+import com.procurement.access.lib.functional.Result
+import com.procurement.access.lib.functional.asSuccess
+
+val allowedPmd = ProcurementMethod.values()
+    .filter {
+        when (it) {
+            ProcurementMethod.DA, ProcurementMethod.TEST_DA,
+            ProcurementMethod.NP, ProcurementMethod.TEST_NP,
+            ProcurementMethod.CD, ProcurementMethod.TEST_CD,
+            ProcurementMethod.IP, ProcurementMethod.TEST_IP -> true //TODO add CD
+
+            ProcurementMethod.OT, ProcurementMethod.TEST_OT,
+            ProcurementMethod.SV, ProcurementMethod.TEST_SV,
+            ProcurementMethod.MV, ProcurementMethod.TEST_MV,
+            ProcurementMethod.DC, ProcurementMethod.TEST_DC,
+            ProcurementMethod.OP, ProcurementMethod.TEST_OP,
+            ProcurementMethod.GPA, ProcurementMethod.TEST_GPA,
+            ProcurementMethod.RT, ProcurementMethod.TEST_RT,
+            ProcurementMethod.MC, ProcurementMethod.TEST_MC,
+            ProcurementMethod.DCO, ProcurementMethod.TEST_DCO,
+            ProcurementMethod.RFQ, ProcurementMethod.TEST_RFQ,
+            ProcurementMethod.CF, ProcurementMethod.TEST_CF,
+            ProcurementMethod.OF, ProcurementMethod.TEST_OF,
+            ProcurementMethod.FA, ProcurementMethod.TEST_FA -> false
+        }
+    }.toSet()
+
+val allowedOperationType = OperationType.allowedElements
+    .filter {
+        when (it) {
+            OperationType.CREATE_AWARD -> true
+            OperationType.AMEND_FE,
+            OperationType.APPLY_QUALIFICATION_PROTOCOL,
+            OperationType.COMPLETE_QUALIFICATION,
+            OperationType.CREATE_CN,
+            OperationType.CREATE_CN_ON_PIN,
+            OperationType.CREATE_CN_ON_PN,
+            OperationType.CREATE_FE,
+            OperationType.CREATE_NEGOTIATION_CN_ON_PN,
+            OperationType.CREATE_PCR,
+            OperationType.CREATE_PIN,
+            OperationType.CREATE_PIN_ON_PN,
+            OperationType.CREATE_PN,
+            OperationType.CREATE_SUBMISSION,
+            OperationType.OUTSOURCING_PN,
+            OperationType.QUALIFICATION,
+            OperationType.QUALIFICATION_CONSIDERATION,
+            OperationType.QUALIFICATION_PROTOCOL,
+            OperationType.RELATION_AP,
+            OperationType.START_SECONDSTAGE,
+            OperationType.SUBMISSION_PERIOD_END,
+            OperationType.TENDER_PERIOD_END,
+            OperationType.UPDATE_AP,
+            OperationType.UPDATE_CN,
+            OperationType.UPDATE_PN,
+            OperationType.WITHDRAW_QUALIFICATION_PROTOCOL -> false
+        }
+    }.toSet()
+
+fun CheckLotsStateRequest.convert(): Result<CheckLotsStateParams, DataErrors> {
+    val cpidParsed = parseCpid(cpid).onFailure { return it }
+    val ocidParsed = parseOcid(ocid).onFailure { return it }
+    val pmdParsed = parsePmd(pmd, allowedPmd).onFailure { return it }
+    val operationTypeParsed = parseOperationType(pmd, allowedOperationType).onFailure { return it }
+    val tender = tender.convert().onFailure { return it }
+
+    return CheckLotsStateParams(
+        cpid = cpidParsed,
+        ocid = ocidParsed,
+        pmd = pmdParsed,
+        operationType = operationTypeParsed,
+        country = country,
+        tender = tender
+    ).asSuccess()
+}
+
+fun CheckLotsStateRequest.Tender.convert(): Result<CheckLotsStateParams.Tender, DataErrors> {
+    val lots = lots.mapResult { parseLotId(it.id, "tender.lots.id") }
+        .onFailure { return it }
+        .map { lotId -> CheckLotsStateParams.Tender.Lot(lotId) }
+
+    return CheckLotsStateParams.Tender(lots).asSuccess()
+}
