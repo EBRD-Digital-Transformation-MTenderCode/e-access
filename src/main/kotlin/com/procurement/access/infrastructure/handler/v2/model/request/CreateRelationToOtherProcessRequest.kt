a6e835f774adf7c28d7f6ce284ade42d4491b5da
katherine <kate.shaidurova@gmail.com>
2020-12-07 17:07:19 +0200

    ES-4978 Added relatedOcid

--- src/main/kotlin/com/procurement/access/infrastructure/handler/v2/model/request/CreateRelationToOtherProcessRequest.kt
+++ src/main/kotlin/com/procurement/access/infrastructure/handler/v2/model/request/CreateRelationToOtherProcessRequest.kt
@@ -1,10 +1,15 @@
 package com.procurement.access.infrastructure.handler.v2.model.request
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
 class CreateRelationToOtherProcessRequest(
     @field:JsonProperty("cpid") @param:JsonProperty("cpid") val cpid: String,
     @field:JsonProperty("ocid") @param:JsonProperty("ocid") val ocid: String,
     @field:JsonProperty("relatedCpid") @param:JsonProperty("relatedCpid") val relatedCpid: String,
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @field:JsonProperty("relatedOcid") @param:JsonProperty("relatedOcid") val relatedOcid: String?,
+
     @field:JsonProperty("operationType") @param:JsonProperty("operationType") val operationType: String
 )
