b507c8616f829c57db98b782095575beb0d5638c
katherine <kate.shaidurova@gmail.com>
2021-01-29 14:38:24 +0200

    ES-5425 Updated parameter types of result

--- src/main/kotlin/com/procurement/access/infrastructure/handler/v2/model/response/DivideLotResult.kt
+++ src/main/kotlin/com/procurement/access/infrastructure/handler/v2/model/response/DivideLotResult.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.procurement.access.domain.model.amount.Amount
 import com.procurement.access.domain.model.enums.LotStatus
 import com.procurement.access.domain.model.enums.LotStatusDetails
-import com.procurement.access.domain.model.lot.LotId
+import com.procurement.access.domain.model.enums.Scheme
 import java.math.BigDecimal
 import java.time.LocalDateTime
 
@@ -18,7 +18,7 @@ data class DivideLotResult(
         @param:JsonProperty("items") @field:JsonProperty("items") val items: List<Item>
     ) {
         data class Lot(
-            @param:JsonProperty("id") @field:JsonProperty("id") val id: LotId,
+            @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
 
             @JsonInclude(JsonInclude.Include.NON_NULL)
             @param:JsonProperty("internalId") @field:JsonProperty("internalId") val internalId: String?,
@@ -29,14 +29,9 @@ data class DivideLotResult(
             @JsonInclude(JsonInclude.Include.NON_NULL)
             @param:JsonProperty("description") @field:JsonProperty("description") val description: String?,
 
-            @JsonInclude(JsonInclude.Include.NON_NULL)
-            @param:JsonProperty("value") @field:JsonProperty("value") val value: Value?,
-
-            @JsonInclude(JsonInclude.Include.NON_NULL)
-            @param:JsonProperty("contractPeriod") @field:JsonProperty("contractPeriod") val contractPeriod: ContractPeriod?,
-
-            @JsonInclude(JsonInclude.Include.NON_NULL)
-            @param:JsonProperty("placeOfPerformance") @field:JsonProperty("placeOfPerformance") val placeOfPerformance: PlaceOfPerformance?,
+            @param:JsonProperty("value") @field:JsonProperty("value") val value: Value,
+            @param:JsonProperty("contractPeriod") @field:JsonProperty("contractPeriod") val contractPeriod: ContractPeriod,
+            @param:JsonProperty("placeOfPerformance") @field:JsonProperty("placeOfPerformance") val placeOfPerformance: PlaceOfPerformance,
 
             @param:JsonProperty("status") @field:JsonProperty("status") val status: LotStatus,
             @param:JsonProperty("statusDetails") @field:JsonProperty("statusDetails") val statusDetails: LotStatusDetails
@@ -111,17 +106,17 @@ data class DivideLotResult(
             @param:JsonProperty("quantity") @field:JsonProperty("quantity") val quantity: BigDecimal,
             @param:JsonProperty("unit") @field:JsonProperty("unit") val unit: Unit,
             @param:JsonProperty("description") @field:JsonProperty("description") val description: String,
-            @param:JsonProperty("relatedLot") @field:JsonProperty("relatedLot") val relatedLot: LotId
+            @param:JsonProperty("relatedLot") @field:JsonProperty("relatedLot") val relatedLot: String
         ) {
             data class Classification(
                 @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
-                @param:JsonProperty("scheme") @field:JsonProperty("scheme") val scheme: String,
+                @param:JsonProperty("scheme") @field:JsonProperty("scheme") val scheme: Scheme,
                 @param:JsonProperty("description") @field:JsonProperty("description") val description: String
             )
 
             data class AdditionalClassification(
                 @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
-                @param:JsonProperty("scheme") @field:JsonProperty("scheme") val scheme: String,
+                @param:JsonProperty("scheme") @field:JsonProperty("scheme") val scheme: Scheme,
                 @param:JsonProperty("description") @field:JsonProperty("description") val description: String
             )
 
