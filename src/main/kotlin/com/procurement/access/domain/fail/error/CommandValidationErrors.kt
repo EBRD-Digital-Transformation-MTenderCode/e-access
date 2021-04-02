89d633110687ab6174b154746bd523bf65985ca5
katherine <kate.shaidurova@gmail.com>
2021-04-02 19:09:31 +0300

    ES-5869 Added error, fixed numberErrors

--- src/main/kotlin/com/procurement/access/domain/fail/error/CommandValidationErrors.kt
+++ src/main/kotlin/com/procurement/access/domain/fail/error/CommandValidationErrors.kt
@@ -2,6 +2,8 @@ package com.procurement.access.domain.fail.error
 
 import com.procurement.access.application.service.Logger
 import com.procurement.access.domain.fail.Fail
+import com.procurement.access.domain.model.Cpid
+import com.procurement.access.domain.model.Ocid
 
 abstract class CommandValidationErrors(
     numberError: String,
@@ -21,38 +23,43 @@ abstract class CommandValidationErrors(
             description = "Expected number of lots: 1. Actual: '$numberOfLots'."
         )
 
-        class InvalidCurrency(expectedCurrency: String, receivedCurrency: String) : CommandValidationErrors(
+        class PnNotFound(cpid: Cpid, ocid: Ocid) : CommandValidationErrors(
             numberError = "46.1.2",
+            description = "Pn record by cpid '$cpid' and ocid '$ocid' not found."
+        )
+
+        class InvalidCurrency(expectedCurrency: String, receivedCurrency: String) : CommandValidationErrors(
+            numberError = "46.1.3",
             description = "Expected currency: '$expectedCurrency'. Actual: '$receivedCurrency'."
         )
 
         class InvalidContractPeriod() : CommandValidationErrors(
-            numberError = "46.1.3",
+            numberError = "46.1.4",
             description = "Contract period start date must precede contract period end date."
         )
 
         class InvalidTenderPeriod() : CommandValidationErrors(
-            numberError = "46.1.4",
+            numberError = "46.1.5",
             description = "Tender period end date must precede contract period start date."
         )
 
         class DuplicatedItemId(itemId: String) : CommandValidationErrors(
-            numberError = "46.1.5",
+            numberError = "46.1.6",
             description = "Items contain duplicate id '$itemId'."
         )
         
         class UnknownRelatedLot(itemId: String, relatedLot: String) : CommandValidationErrors(
-            numberError = "46.1.6",
+            numberError = "46.1.7",
             description = "Item '$itemId' contain unknown relatedLot '$relatedLot'."
         )
 
         class ElectronicAuctionsAreMissing() : CommandValidationErrors(
-            numberError = "46.1.7",
+            numberError = "46.1.8",
             description = "Electronic auctions are missing."
         )
 
         class RedundantElectronicAuctions() : CommandValidationErrors(
-            numberError = "46.1.8",
+            numberError = "46.1.9",
             description = "Request contains redundant electronic auctions."
         )
     }
