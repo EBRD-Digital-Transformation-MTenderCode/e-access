7e8ed90325b92fe3a7dedcdaec9c9c32d1618db2
katherine <kate.shaidurova@gmail.com>
2021-04-02 18:35:01 +0300

    ES-5869 Added validation errors

--- src/main/kotlin/com/procurement/access/domain/fail/error/CommandValidationErrors.kt
+++ src/main/kotlin/com/procurement/access/domain/fail/error/CommandValidationErrors.kt
@@ -14,4 +14,46 @@ abstract class CommandValidationErrors(
     override fun logging(logger: Logger) {
         logger.error(message = message)
     }
+
+    object ValidateRfqData {
+        class InvalidNumberOfLots(numberOfLots: Int) : CommandValidationErrors(
+            numberError = "46.1.1",
+            description = "Expected number of lots: 1. Actual: '$numberOfLots'."
+        )
+
+        class InvalidCurrency(expectedCurrency: String, receivedCurrency: String) : CommandValidationErrors(
+            numberError = "46.1.2",
+            description = "Expected currency: '$expectedCurrency'. Actual: '$receivedCurrency'."
+        )
+
+        class InvalidContractPeriod() : CommandValidationErrors(
+            numberError = "46.1.3",
+            description = "Contract period start date must precede contract period end date."
+        )
+
+        class InvalidTenderPeriod() : CommandValidationErrors(
+            numberError = "46.1.4",
+            description = "Tender period end date must precede contract period start date."
+        )
+
+        class DuplicatedItemId(itemId: String) : CommandValidationErrors(
+            numberError = "46.1.5",
+            description = "Items contain duplicate id '$itemId'."
+        )
+        
+        class UnknownRelatedLot(itemId: String, relatedLot: String) : CommandValidationErrors(
+            numberError = "46.1.6",
+            description = "Item '$itemId' contain unknown relatedLot '$relatedLot'."
+        )
+
+        class ElectronicAuctionsAreMissing() : CommandValidationErrors(
+            numberError = "46.1.7",
+            description = "Electronic auctions are missing."
+        )
+
+        class RedundantElectronicAuctions() : CommandValidationErrors(
+            numberError = "46.1.8",
+            description = "Request contains redundant electronic auctions."
+        )
+    }
 }
