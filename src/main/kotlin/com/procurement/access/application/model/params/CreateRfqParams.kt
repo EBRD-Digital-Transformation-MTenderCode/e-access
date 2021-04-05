d045478e1ca0786f69136f8e68a0fe66e5ac96f9
katherine <kate.shaidurova@gmail.com>
2021-04-05 16:27:20 +0300

    ES-5906 Made PlaceOfPerformance.description optional

--- src/main/kotlin/com/procurement/access/application/model/params/CreateRfqParams.kt
+++ src/main/kotlin/com/procurement/access/application/model/params/CreateRfqParams.kt
@@ -41,7 +41,7 @@ data class CreateRfqParams(
             )
 
             data class PlaceOfPerformance(
-                val description: String,
+                val description: String?,
 
                 val address: Address
             ) {
