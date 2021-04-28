fa962b0df344ec91bd347c35aca5953e1d862c8f
katherine <kate.shaidurova@gmail.com>
2021-04-28 19:15:12 +0300

    ES-5983 Added DTOs

---
+++ src/main/kotlin/com/procurement/access/infrastructure/handler/v2/model/request/GetBuyersOwnersRequest.kt
@@ -0,0 +1,9 @@
+package com.procurement.access.infrastructure.handler.v2.model.request
+
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class GetBuyersOwnersRequest(
+    @param:JsonProperty("cpid") @field:JsonProperty("cpid") val cpid: String,
+    @param:JsonProperty("ocid") @field:JsonProperty("ocid") val ocid: String
+)
