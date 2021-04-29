ee5713ab9e9ff1045d20a99815f3fcb248a5e678
katherine <kate.shaidurova@gmail.com>
2021-04-29 16:43:06 +0300

    ES-5983 Added handler and its call

---
+++ src/main/kotlin/com/procurement/access/infrastructure/handler/v2/GetBuyersOwnersHandler.kt
@@ -0,0 +1,35 @@
+package com.procurement.access.infrastructure.handler.v2
+
+import com.procurement.access.application.service.Logger
+import com.procurement.access.application.service.Transform
+import com.procurement.access.domain.fail.Fail
+import com.procurement.access.infrastructure.api.v2.CommandTypeV2
+import com.procurement.access.infrastructure.handler.HistoryRepository
+import com.procurement.access.infrastructure.handler.v2.base.AbstractHistoricalHandler
+import com.procurement.access.infrastructure.handler.v2.converter.convert
+import com.procurement.access.infrastructure.handler.v2.model.request.GetBuyersOwnersRequest
+import com.procurement.access.infrastructure.handler.v2.model.response.GetBuyersOwnersResult
+import com.procurement.access.lib.functional.Result
+import com.procurement.access.lib.functional.flatMap
+import com.procurement.access.service.TenderService
+import org.springframework.stereotype.Service
+
+@Service
+class GetBuyersOwnersHandler(
+    private val tenderService: TenderService,
+    transform: Transform,
+    historyRepository: HistoryRepository,
+    logger: Logger
+) : AbstractHistoricalHandler<GetBuyersOwnersResult>(transform, historyRepository, logger) {
+
+    override val action: CommandTypeV2
+        get() = CommandTypeV2.GET_BUYERS_OWNERS
+
+    override fun execute(descriptor: CommandDescriptor): Result<GetBuyersOwnersResult, Fail> {
+        val params = descriptor.body.asJsonNode
+            .params<GetBuyersOwnersRequest>()
+            .flatMap { it.convert() }
+            .onFailure { return it }
+        return tenderService.getBuyersOwners(params = params)
+    }
+}
