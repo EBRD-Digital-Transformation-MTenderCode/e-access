95caace7707b514b059a4d3f9ff45f20ee4c5fce
katherine <kate.shaidurova@gmail.com>
2021-06-16 12:02:35 +0300

    ES-6245 Added ocid to request params of CheckExistenceFA

--- src/main/kotlin/com/procurement/access/application/model/params/CheckExistenceFAParams.kt
+++ src/main/kotlin/com/procurement/access/application/model/params/CheckExistenceFAParams.kt
@@ -1,20 +1,25 @@
 package com.procurement.access.application.model.params
 
 import com.procurement.access.application.model.parseCpid
+import com.procurement.access.application.model.parseOcid
 import com.procurement.access.domain.fail.error.DataErrors
 import com.procurement.access.domain.model.Cpid
+import com.procurement.access.domain.model.Ocid
 import com.procurement.access.lib.functional.Result
 import com.procurement.access.lib.functional.asSuccess
 
 data class CheckExistenceFAParams private constructor(
-    val cpid: Cpid
+    val cpid: Cpid,
+    val ocid: Ocid
 ) {
     companion object {
-        fun tryCreate(cpid: String): Result<CheckExistenceFAParams, DataErrors> {
+        fun tryCreate(cpid: String, ocid: String): Result<CheckExistenceFAParams, DataErrors> {
             val cpidParsed = parseCpid(value = cpid)
                 .onFailure { error -> return error }
+            val ocidParsed = parseOcid(value = ocid)
+                .onFailure { error -> return error }
 
-            return CheckExistenceFAParams(cpid = cpidParsed).asSuccess()
+            return CheckExistenceFAParams(cpid = cpidParsed, ocid = ocidParsed).asSuccess()
         }
     }
 }
