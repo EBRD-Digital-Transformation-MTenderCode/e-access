d5a17630a19a65efac69b5d7ca6bf0f25e318f0f
katherine <kate.shaidurova@gmail.com>
2021-06-18 12:05:03 +0300

    ES-6245 Fixed GetAPTitleAndDescriptionContext.kt

--- src/main/kotlin/com/procurement/access/application/service/ap/get/GetAPTitleAndDescriptionContext.kt
+++ src/main/kotlin/com/procurement/access/application/service/ap/get/GetAPTitleAndDescriptionContext.kt
@@ -1,6 +1,9 @@
 package com.procurement.access.application.service.ap.get
 
+import com.procurement.access.domain.model.Ocid
+
 data class GetAPTitleAndDescriptionContext(
     val cpid: String,
+    val ocid: Ocid,
     val stage: String
 )
