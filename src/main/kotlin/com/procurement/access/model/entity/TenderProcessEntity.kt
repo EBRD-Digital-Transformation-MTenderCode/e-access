a101df9ebb45f1e482c6fc885016d68e72ae647b
katherine <kate.shaidurova@gmail.com>
2021-06-23 13:42:58 +0300

    ES-6245 Replaced string ocid and cpid with concrete types in TenderProcessEntity.kt

--- src/main/kotlin/com/procurement/access/model/entity/TenderProcessEntity.kt
+++ src/main/kotlin/com/procurement/access/model/entity/TenderProcessEntity.kt
@@ -1,18 +1,20 @@
 package com.procurement.access.model.entity
 
+import com.procurement.access.domain.model.Cpid
+import com.procurement.access.domain.model.Ocid
 import java.time.LocalDateTime
 import java.util.*
 
 
 data class TenderProcessEntity(
 
-    var cpId: String,
+    var cpId: Cpid,
 
     var token: UUID,
 
     var owner: String,
 
-    var ocid: String,
+    var ocid: Ocid,
 
     var createdDate: LocalDateTime,
 
