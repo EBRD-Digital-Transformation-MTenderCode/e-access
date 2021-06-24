16f9023d74abb208258f712d4e970e07f7d01bf9
katherine <kate.shaidurova@gmail.com>
2021-06-24 12:24:40 +0300

    ES-6285 Made ocid SingleStage, removed redundant annotations

--- src/main/kotlin/com/procurement/access/application/model/params/GetDataForContractParams.kt
+++ src/main/kotlin/com/procurement/access/application/model/params/GetDataForContractParams.kt
@@ -1,17 +1,15 @@
 package com.procurement.access.application.model.params
 
-
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.procurement.access.domain.model.Cpid
 import com.procurement.access.domain.model.Ocid
 import com.procurement.access.domain.model.lot.LotId
 
 data class GetDataForContractParams(
-    @param:JsonProperty("relatedCpid") @field:JsonProperty("relatedCpid") val relatedCpid: Cpid,
-    @param:JsonProperty("relatedOcid") @field:JsonProperty("relatedOcid") val relatedOcid: Ocid,
-    @param:JsonProperty("awards") @field:JsonProperty("awards") val awards: List<Award>
+    val relatedCpid: Cpid,
+    val relatedOcid: Ocid.SingleStage,
+    val awards: List<Award>
 ) {
     data class Award(
-        @param:JsonProperty("relatedLots") @field:JsonProperty("relatedLots") val relatedLots: List<LotId>
+        val relatedLots: List<LotId>
     )
 }
